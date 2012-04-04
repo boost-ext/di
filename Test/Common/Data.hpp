1aafa6fc9f03cc546c8b71df5dae4241e4e4e621
Krzysztof Jusiak <krzysztof@jusiak.net>
2012-04-04 15:11:08 +0200

    cosmetics

--- Test/Common/Data.hpp
+++ Test/Common/Data.hpp
@@ -11,6 +11,7 @@
 #include <boost/mpl/string.hpp>
 #include "QDeps/Front/Ctor.hpp"
 #include "QDeps/Utility/Named.hpp"
+#include "QDeps/Utility/Provider.hpp"
 
 namespace QDeps
 {
@@ -18,6 +19,9 @@ namespace Test
 {
 namespace Common
 {
+
+using namespace boost;
+using namespace Utility;
 
 struct If0
 {
@@ -38,6 +42,18 @@ struct CIf01 : If0
 struct CIf02 : If0
 {
     virtual void dummy() { }
+};
+
+struct CIf03 : If0
+{
+    QDEPS_CTOR(CIf03, int i, double d)
+        : i(i), d(d)
+    { }
+
+    virtual void dummy() { }
+
+    int i;
+    double d;
 };
 
 struct C0
@@ -72,56 +88,56 @@ struct C3
 
 struct C4
 {
-    QDEPS_CTOR(C4, boost::shared_ptr<C3> c3, Utility::Named<int, boost::mpl::string<'1'> > i1, Utility::Named<int, boost::mpl::string<'2'> > i2)
+    QDEPS_CTOR(C4, shared_ptr<C3> c3, Named<int, mpl::string<'1'> > i1, Named<int, mpl::string<'2'> > i2)
         : c3(c3), i1(i1), i2(i2)
     { }
 
-    boost::shared_ptr<C3> c3;
+    shared_ptr<C3> c3;
     int i1;
     int i2;
 };
 
 struct C5
 {
-    QDEPS_CTOR(C5, boost::shared_ptr<If0> if0, boost::shared_ptr<C2> c2, boost::shared_ptr<C1> c1)
+    QDEPS_CTOR(C5, shared_ptr<If0> if0, shared_ptr<C2> c2, shared_ptr<C1> c1)
         : if0(if0), c2(c2), c1(c1)
     { }
 
-    boost::shared_ptr<If0> if0;
-    boost::shared_ptr<C2> c2;
-    boost::shared_ptr<C1> c1;
+    shared_ptr<If0> if0;
+    shared_ptr<C2> c2;
+    shared_ptr<C1> c1;
 };
 
 struct C6
 {
-    QDEPS_CTOR(C6, boost::shared_ptr<C3> c3, const boost::shared_ptr<C4>& c4, C5 c5)
+    QDEPS_CTOR(C6, shared_ptr<C3> c3, const shared_ptr<C4>& c4, C5 c5)
         : c3(c3), c4(c4), c5(c5)
     { }
 
-    boost::shared_ptr<C3> c3;
-    boost::shared_ptr<C4> c4;
+    shared_ptr<C3> c3;
+    shared_ptr<C4> c4;
     C5 c5;
 };
 
 struct C7
 {
-    QDEPS_CTOR(C7, boost::shared_ptr<If0> if0, boost::shared_ptr<C6> c6)
+    QDEPS_CTOR(C7, shared_ptr<If0> if0, shared_ptr<C6> c6)
         : if0(if0), c6(c6)
     { }
 
-    boost::shared_ptr<If0> if0;
-    boost::shared_ptr<C6> c6;
+    shared_ptr<If0> if0;
+    shared_ptr<C6> c6;
 };
 
 struct C8
 {
-    QDEPS_CTOR(C8, boost::shared_ptr<C7> c7, C0 c0, boost::shared_ptr<C1> c1, int i)
+    QDEPS_CTOR(C8, shared_ptr<C7> c7, C0 c0, shared_ptr<C1> c1, int i)
         : c7(c7), c0(c0), c1(c1), i(i)
     { }
 
-    boost::shared_ptr<C7> c7;
+    shared_ptr<C7> c7;
     C0 c0;
-    boost::shared_ptr<C1> c1;
+    shared_ptr<C1> c1;
     int i;
 };
 
@@ -136,7 +152,7 @@ struct C9 : C2
 
 struct C10
 {
-    QDEPS_CTOR_TRAITS(Utility::Named<int, boost::mpl::string<'1'> >, Utility::Named<int, boost::mpl::string<'2'> >);
+    QDEPS_CTOR_TRAITS(Named<int, mpl::string<'1'> >, Named<int, mpl::string<'2'> >);
 
     C10(int i1, int i2)
         : i1(i1), i2(i2)
@@ -148,12 +164,12 @@ struct C10
 
 struct C11
 {
-    QDEPS_CTOR(C11, Utility::Named<const If0&, boost::mpl::string<'1'> > if0, Utility::Named< boost::shared_ptr<int>, boost::mpl::string<'2'> > i)
+    QDEPS_CTOR(C11, Named<const If0&, mpl::string<'1'> > if0, Named< shared_ptr<int>, mpl::string<'2'> > i)
         : if0(if0), i(i)
     { }
 
     const If0& if0;
-    boost::shared_ptr<int> i;
+    shared_ptr<int> i;
 };
 
 struct CD2;
@@ -182,6 +198,50 @@ struct CD4
 struct CD5
 {
     QDEPS_CTOR(CD5, CD4*) { };
+};
+
+struct Transaction
+{
+    static int& inc()
+    {
+        static int counter = 0;
+        counter++;
+        return counter;
+    }
+
+    Transaction(int i)
+        : i(i + inc())
+    { }
+
+    int i;
+};
+
+struct TransactionProvider : Provider<Transaction>
+{
+    QDEPS_CTOR(TransactionProvider, shared_ptr<C3> c3)
+        : c3(c3)
+    { }
+
+    virtual Transaction get() const
+    {
+        return Transaction(c3->i);
+    }
+
+    shared_ptr<C3> c3;
+};
+
+struct TransactionUsage
+{
+    QDEPS_CTOR(TransactionUsage, shared_ptr< Provider<Transaction> > p)
+        : p(p)
+    { }
+
+    int getId() const
+    {
+        return p->get().i;
+    }
+
+    shared_ptr< Provider<Transaction> > p;
 };
 
 } // namespace Common
