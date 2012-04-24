dc5ddac77d03b0c48b40a998143a1d746417a577
Krzysztof Jusiak <krzysztof@jusiak.net>
2012-04-24 14:18:56 +0200

    module

---
+++ boost/di/aux/module.hpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2012 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef BOOST_DI_AUX_MODULE_HPP
+#define BOOST_DI_AUX_MODULE_HPP
+
+namespace boost {
+namespace di {
+namespace aux {
+
+class module { };
+
+} // namespace aux
+} // namespace di
+} // namespace boost
+
+#endif
+
