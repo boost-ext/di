54c1fbfde3e50c9ad239656aa5c69c0ac6b8f030
Krzysztof Jusiak <krzysztof@jusiak.net>
2012-07-09 23:05:21 +0200

    compile clean

--- libs/di/test/scopes/explicit_ut.cpp
+++ libs/di/test/scopes/explicit_ut.cpp
@@ -4,7 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#include "boost/di/detail/explicit_value.hpp"
+#include "boost/di/scopes/explicit_.hpp"
 
 #include <boost/test/unit_test.hpp>
 #include <boost/mpl/vector.hpp>
@@ -12,7 +12,7 @@
 
 namespace boost {
 namespace di {
-namespace detail {
+namespace scopes {
 
 struct empty { };
 struct double_value
@@ -22,30 +22,30 @@ struct double_value
 double double_value::value = 0.0;
 
 BOOST_AUTO_TEST_CASE(value_generic) {
-    BOOST_CHECK(!explicit_value<int>::value);
-    BOOST_CHECK(!explicit_value<empty>::value);
+    BOOST_CHECK(!explicit_<int>::value);
+    BOOST_CHECK(!explicit_<empty>::value);
 }
 
 BOOST_AUTO_TEST_CASE(value_mpl_int) {
     const int i = 42;
-    BOOST_CHECK((explicit_value<mpl::int_<0> >::value));
-    BOOST_CHECK_EQUAL(i, explicit_value<mpl::int_<i> >::create());
+    BOOST_CHECK((explicit_<mpl::int_<0> >::value));
+    BOOST_CHECK_EQUAL(i, explicit_<mpl::int_<i> >::create());
 }
 
 BOOST_AUTO_TEST_CASE(value_mpl_string) {
     const std::string s = "s";
-    BOOST_CHECK((explicit_value<mpl::string<'s'> >::value));
-    BOOST_CHECK_EQUAL(s, explicit_value<mpl::string<'s'> >::create());
+    BOOST_CHECK((explicit_<mpl::string<'s'> >::value));
+    BOOST_CHECK_EQUAL(s, explicit_<mpl::string<'s'> >::create());
 }
 
 BOOST_AUTO_TEST_CASE(value_has_value_type) {
     const double d = 42.0;
     double_value::value = d;
-    BOOST_CHECK((explicit_value<double_value>::value));
-    BOOST_CHECK_EQUAL(d, explicit_value<double_value>::create());
+    BOOST_CHECK((explicit_<double_value>::value));
+    BOOST_CHECK_EQUAL(d, explicit_<double_value>::create());
 }
 
-} // namespace detail
+} // namespace scopes
 } // namespace di
 } // namespace boost
 
