98b1af0921196a1ef05504d4165e0d0b59e2fa41
Krzysztof Jusiak <krzysztof@jusiak.net>
2012-07-10 19:22:37 +0200

    scopes refactor: explicit

--- libs/di/test/scopes/explicit_ut.cpp
+++ libs/di/test/scopes/explicit_ut.cpp
@@ -22,27 +22,27 @@ struct double_value
 double double_value::value = 0.0;
 
 BOOST_AUTO_TEST_CASE(value_generic) {
-    BOOST_CHECK(!explicit_<int>::value);
-    BOOST_CHECK(!explicit_<empty>::value);
+    BOOST_CHECK(!aux::explicit_impl<int>::value);
+    BOOST_CHECK(!aux::explicit_impl<empty>::value);
 }
 
 BOOST_AUTO_TEST_CASE(value_mpl_int) {
     const int i = 42;
-    BOOST_CHECK((explicit_<mpl::int_<0> >::value));
-    BOOST_CHECK_EQUAL(i, explicit_<mpl::int_<i> >::create());
+    BOOST_CHECK((aux::explicit_impl<mpl::int_<0> >::value));
+    BOOST_CHECK_EQUAL(i, aux::explicit_impl<mpl::int_<i> >::create());
 }
 
 BOOST_AUTO_TEST_CASE(value_mpl_string) {
     const std::string s = "s";
-    BOOST_CHECK((explicit_<mpl::string<'s'> >::value));
-    BOOST_CHECK_EQUAL(s, explicit_<mpl::string<'s'> >::create());
+    BOOST_CHECK((aux::explicit_impl<mpl::string<'s'> >::value));
+    BOOST_CHECK_EQUAL(s, aux::explicit_impl<mpl::string<'s'> >::create());
 }
 
 BOOST_AUTO_TEST_CASE(value_has_value_type) {
     const double d = 42.0;
     double_value::value = d;
-    BOOST_CHECK((explicit_<double_value>::value));
-    BOOST_CHECK_EQUAL(d, explicit_<double_value>::create());
+    BOOST_CHECK((aux::explicit_impl<double_value>::value));
+    BOOST_CHECK_EQUAL(d, aux::explicit_impl<double_value>::create());
 }
 
 } // namespace scopes
