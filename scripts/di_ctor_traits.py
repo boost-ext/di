694ac9cb0b70dff53d38676e47561bcc668d608c
Krzysztof Jusiak <krzysztof@jusiak.net>
2013-08-28 16:08:07 +0200

    di ctor traits script

--- scripts/di_ctor_traits.py
+++ scripts/di_ctor_traits.py
@@ -1,132 +1,289 @@
 #!/usr/bin/env python
 
+import os
 import sys
-#sys.path.append(".")
-#import pprint
-#import ctypes
-#import re
-#from clang.cindex import Index
-#from clang.cindex import CursorKind
-#from clang.cindex import Type
-#from clang.cindex import TypeKind
-#from clang.cindex import TranslationUnit
-#from optparse import OptionParser, OptionGroup
-#import pyclang.cindex as clang
+from optparse import OptionParser
+from clang.cindex import Index
+from clang.cindex import TranslationUnit
+from clang.cindex import Cursor
+from clang.cindex import CursorKind
 
-from clang.cindex import *
-  
-di_ctor_traits = """
-    #ifndef BOOST_DI_CTOR_TRAITS
-    #define BOOST_DI_CTOR_TRAITS
+if sys.version_info < (3, 0):
+    import __builtin__
+    def str(object, unused = None):
+        return __builtin__.str(object)
 
-    %(includes)s
+    def bytes(object, unused = None):
+        return __builtin__.bytes(object)
 
-    namespace boost {
-    namespace di {
+class mock_method:
+    operators = {
+        'operator,'   : 'comma_operator',
+        'operator!'   : 'logical_not_operator',
+        'operator!='  : 'inequality_operator',
+        'operator%'   : 'modulus_operator',
+        'operator%='  : 'modulus_assignment_operator',
+        'operator&'   : 'address_of_or_bitwise_and_operator',
+        'operator&&'  : 'logical_and_operator',
+        'operator&='  : 'bitwise_and_assignment_operator',
+        'operator()'  : 'function_call_or_cast_operator',
+        'operator*'   : 'multiplication_or_dereference_operator',
+        'operator*='  : 'multiplication_assignment_operator',
+        'operator+'   : 'addition_or_unary_plus_operator',
+        'operator++'  : 'increment1_operator',
+        'operator+='  : 'addition_assignment_operator',
+        'operator-'   : 'subtraction_or_unary_negation_operator',
+        'operator--'  : 'decrement1_operator',
+        'operator-='  : 'subtraction_assignment_operator',
+        'operator->'  : 'member_selection_operator',
+        'operator->*' : 'pointer_to_member_selection_operator',
+        'operator/'   : 'division_operator',
+        'operator/='  : 'division_assignment_operator',
+        'operator<'   : 'less_than_operator',
+        'operator<<'  : 'left_shift_operator',
+        'operator<<=' : 'left_shift_assignment_operator',
+        'operator<='  : 'less_than_or_equal_to_operator',
+        'operator='   : 'assignment_operator',
+        'operator=='  : 'equality_operator',
+        'operator>'   : 'greater_than_operator',
+        'operator>='  : 'greater_than_or_equal_to_operator',
+        'operator>>'  : 'right_shift_operator',
+        'operator>>=' : 'right_shift_assignment_operator',
+        'operator[]'  : 'array_subscript_operator',
+        'operator^'   : 'exclusive_or_operator',
+        'operator^='  : 'exclusive_or_assignment_operator',
+        'operator|'   : 'bitwise_inclusive_or_operator',
+        'operator|='  : 'bitwise_inclusive_or_assignment_operator',
+        'operator||'  : 'logical_or_operator',
+        'operator~'   : 'complement_operator'
+    }
 
-        %(ctor_traits)s
+    def __init__(self, result_type, name, is_const, args_size, args, args_prefix = 'arg'):
+        self.result_type = result_type
+        self.name = name
+        self.is_const = is_const
+        self.args_size = args_size
+        self.args = args
+        self.args_prefix = args_prefix
 
-    } // namespace di
-    } // namespace boost
+    def __named_args(self):
+        result = []
+        for i in range(0, self.args_size):
+            i and result.append(', ')
+            result.append(self.args_prefix + str(i))
+        return ''.join(result)
 
-    #endif
+    def __named_args_with_types(self):
+        if (self.args == ''):
+            return ''
+        result = []
+        in_type = False
+        i = 0
+        for c in self.args:
+            if c in ['<', '(']:
+                in_type = True
+            elif c in ['>', ')']:
+                in_type = False
+            if not in_type and c == ',':
+                result.append(' ' + self.args_prefix + str(i))
+                i+=1
+            result.append(c)
+        result.append(' ' + self.args_prefix + str(i))
+        return ''.join(result)
 
-"""
+    def to_string(self, gap = '    '):
+        mock = []
+        name = self.name
+        if self.name in self.operators:
+            mock.append(gap)
+            mock.append(
+                "virtual %(result_type)s %(name)s(%(args)s) %(const)s{ %(return)s %(body)s; }\n" % {
+                    'result_type' : self.result_type,
+                    'name' : self.name,
+                    'args' : self.__named_args_with_types(),
+                    'const' : self.is_const and 'const ' or '',
+                    'return' : self.result_type.strip() != 'void' and 'return' or '',
+                    'body' : self.operators[self.name] + "(" + self.__named_args() + ")"
+                }
+            )
+            name = self.operators[self.name]
 
-di_ctor_trait = """
-    template<>
-    struct ctor_traits< %(class)s >
-    {
-        static void ctor(%(args)s);
-    };
-"""
+        mock.append(gap)
+        mock.append(
+            "MOCK_%(const)sMETHOD%(nr)s(%(name)s, %(result_type)s(%(args)s));" % {
+            'const' : self.is_const and 'CONST_' or '',
+            'nr' : self.args_size,
+            'name' : name,
+            'result_type' : self.result_type,
+            'args' : self.args
+        })
 
-di_include = "#include <%(include)s>\n"
+        return ''.join(mock)
 
-def get_ctors(node):
-    ctors = {}
-    get_ctors_impl(ctors, node)
-    return ctors
+class mock_generator:
+    def __is_const_function(self, tokens):
+        for token in reversed(tokens):
+            if token == 'const':
+                return True
+            elif token == ')':
+                return False
+        return False
 
-def get_ctors_impl(ctors, node, namespace = ""):
-    if node.kind == CursorKind.CONSTRUCTOR:
-        #if namespace.startswith("::"):
-        ctors[namespace + "::" + node.spelling] = node.displayname[len(node.spelling) + 1 : -1]
+    def __is_virtual_function(self, tokens):
+        return 'virtual' in [token for token in tokens]
 
-        #for el in node.get_tokens():
-            #if el.spelling == "iii":
-                #loc = SourceLocation.from_position(node.translation_unit, el.location.file, 8, 3)
-                #cursor = Cursor.from_location(node.translation_unit, loc)
-                #print cursor.location.line
-                #print cursor.location.column
-                #print cursor.kind
-                #print cursor.underlying_typedef_type.kind.spelling
+    def __is_pure_virtual_function(self, tokens):
+        return len(tokens) >= 3 and                     \
+               self.__is_virtual_function(tokens) and   \
+               tokens[-3] == '=' and                    \
+               tokens[-2] == '0' and                    \
+               tokens[-1] == ';'
 
-                #print cursor.underlying_typedef_type
+    def __get_result_type(self, tokens, name):
+        assert(self.__is_pure_virtual_function(tokens))
+        result_type = []
+        for token in tokens:
+            if token in [name, 'operator']:
+                break
+            if token not in ['virtual', 'inline', 'volatile']:
+                result_type.append(token)
+            if token in ['const', 'volatile']:
+                result_type.append(' ')
+        return ''.join(result_type)
 
-#                loc2 = SourceLocation.from_position(node.translation_unit, node.location.file, cursor.get_definition().location.line, cursor.get_definition().location.column)
- #               cursor2 = Cursor.from_location(node.translation_unit, loc2)
+    def __pretty_mock_methods(self, mock_methods):
+        result = []
+        for i, mock_method in enumerate(mock_methods):
+            i and result.append('\n')
+            result.append(mock_method.to_string())
+            first = False
+        return ''.join(result)
 
-    elif node.kind == CursorKind.NAMESPACE:
-        namespace += "::" + node.displayname
-        #if namespace.startswith("::"):
-            [get_ctors_impl(ctors, c, namespace) for c in node.get_children()]
-    else:
-        [get_ctors_impl(ctors, c, namespace) for c in node.get_children()]
+    def __pretty_namespaces_begin(self, decl):
+        result = []
+        for i, namespace in enumerate(decl.split("::")[0 : -1]):
+            i and result.append('\n')
+            result.append("namespace " + namespace + " {")
+        return ''.join(result)
 
-def generate_ctor_traits(ctors):
-    result = ""
-    for class_name, args in ctors.items():
-        result += di_ctor_trait % { 'class' : class_name, 'args' : args }
-    return result
+    def __pretty_namespaces_end(self, decl):
+        result = []
+        for i, namespace in enumerate(decl.split("::")[0 : -1]):
+            i and result.append('\n')
+            result.append("} // namespace " + namespace)
+        return ''.join(result)
 
-def generate_includes(includes):
-    result = ''
-    for include in includes:
-        result += di_include % { 'include' : include }
-    return result
+    def __get_mock_methods(self, node, mock_methods, class_decl = ""):
+        name = str(node.displayname, self.encode)
+        if node.kind == CursorKind.CXX_METHOD:
+            tokens = [str(token.spelling, self.encode) for token in node.get_tokens()]
+            spelling = str(node.spelling, self.encode)
+            file = str(node.location.file.name, self.encode)
+            if self.__is_pure_virtual_function(tokens):
+                mock_methods.setdefault(class_decl, [file]).append(
+                    mock_method(
+                         self.__get_result_type(tokens, spelling),
+                         spelling,
+                         self.__is_const_function(tokens),
+                         len(list(node.get_arguments())),
+                         name[len(node.spelling) + 1 : -1]
+                    )
+                )
+        elif node.kind in [CursorKind.CLASS_DECL, CursorKind.NAMESPACE]:
+            class_decl = class_decl == "" and name or class_decl + (name == "" and "" or "::") + name
+            if class_decl.startswith(self.decl):
+                [self.__get_mock_methods(c, mock_methods, class_decl) for c in node.get_children()]
+        else:
+            [self.__get_mock_methods(c, mock_methods, class_decl) for c in node.get_children()]
 
-    #global opts
+    def __generate_file(self, decl, mock_methods, file_type, file_template_type):
+        interface = decl.split("::")[-1]
+        mock_file = {
+            'hpp' : self.mock_file_hpp % { 'interface' : interface },
+            'cpp' : self.mock_file_cpp % { 'interface' : interface },
+        }
+        path = self.path + "/" + mock_file[file_type]
+        not os.path.exists(os.path.dirname(path)) and os.makedirs(os.path.dirname(path))
+        with open(path, 'w') as file:
+            file.write(file_template_type % {
+                'mock_file_hpp' : mock_file['hpp'],
+                'mock_file_cpp' : mock_file['cpp'],
+                'generated_dir' : self.path,
+                'guard' : mock_file[file_type].replace('.', '_').upper(),
+                'dir' : os.path.dirname(mock_methods[0]),
+                'file' : os.path.basename(mock_methods[0]),
+                'namespaces_begin' : self.__pretty_namespaces_begin(decl),
+                'interface' : interface,
+                'mock_methods' : self.__pretty_mock_methods(mock_methods[1:]),
+                'namespaces_end' : self.__pretty_namespaces_end(decl)
+            })
 
-    #parser = OptionParser("usage: %prog [options] {filename} [clang-args*]")
-    #parser.add_option("-i", "--include", dest="include", help="include pattern", default=None);
-    #parser.add_option("-e", "--exclude", dest="exclude", help="exclude pattern", default=None);
-    #parser.add_option("-o", "--output", dest="output", help="output file", default=None);
-    #parser.disable_interspersed_args()
-    #(opts, args) = parser.parse_args()
+    def __parse(self, files, args):
+        tmp_file = b"~.hpp"
+        def generate_includes(includes):
+            result = []
+            for include in includes:
+                result.append("#include \"%(include)s\"\n" % { 'include' : include })
+            return ''.join(result)
 
-    #if len(args) == 0:
-    #    parser.error('invalid number arguments')
+        return Index.create(excludeDecls = True).parse(
+            path = tmp_file
+          , args = args
+          , unsaved_files = [(tmp_file, bytes(generate_includes(files), self.encode))]
+          , options = TranslationUnit.PARSE_SKIP_FUNCTION_BODIES | TranslationUnit.PARSE_INCOMPLETE
+        )
 
-    #print args
-    #print opts.exclude
-    #print opts.include
+    def __init__(self, files, args, decl, path, mock_file_hpp, file_template_hpp, mock_file_cpp, file_template_cpp, encode = "utf-8"):
+        self.decl = decl
+        self.path = path
+        self.mock_file_hpp = mock_file_hpp
+        self.file_template_hpp = file_template_hpp
+        self.mock_file_cpp = mock_file_cpp
+        self.file_template_cpp = file_template_cpp
+        self.encode = encode
+        self.cursor = self.__parse(files, args).cursor
 
-index = Index.create(excludeDecls = True)
+    def generate(self):
+        mock_methods = {}
+        self.__get_mock_methods(self.cursor, mock_methods)
+        for decl, mock_methods in mock_methods.items():
+            if len(mock_methods) > 0:
+                self.file_template_hpp != "" and self.__generate_file(decl, mock_methods, "hpp", self.file_template_hpp)
+                self.file_template_cpp != "" and self.__generate_file(decl, mock_methods, "cpp", self.file_template_cpp)
+        return 0
 
-f = open("di_ctor_traits.hpp", "w")
+def main(args):
+    clang_args = None
+    args_split = [i for i, arg in enumerate(args) if arg == "--"]
+    if args_split:
+        args, clang_args = args[:args_split[0]], args[args_split[0] + 1:]
 
-tu = index.parse(
-    path = "__di__.hpp"
-  , unsaved_files = [("__di__.hpp", generate_includes(sys.argv[1:]))]
-  , options = TranslationUnit.PARSE_SKIP_FUNCTION_BODIES | TranslationUnit.PARSE_INCOMPLETE
-)
+    default_config = os.path.dirname(args[0]) + "/gmock.conf"
 
-if not tu:
-    parser.error("unable to load input")
+    parser = OptionParser(usage="usage: %prog [options] files...")
+    parser.add_option("-c", "--config", dest="config", default=default_config, help="config FILE (default='gmock.conf')", metavar="FILE")
+    parser.add_option("-d", "--dir", dest="path", default=".", help="dir for generated mocks (default='.')", metavar="DIR")
+    parser.add_option("-l", "--limit", dest="decl", default="", help="limit to interfaces within declaration (default='')", metavar="LIMIT")
+    (options, args) = parser.parse_args(args)
 
-f.write(di_ctor_traits % {
-    'includes' : ""
-  , 'ctor_traits' : generate_ctor_traits(get_ctors(tu.cursor))
-})
+    if len(args) == 1:
+        parser.error("at least one file has to be given")
 
-f.close()
+    config = {}
+    with open(options.config, 'r') as file:
+        exec(file.read(), config)
 
-#TODO template classes, ctors
-#TODO 2 or more ctors (the longest one)
-#TODO move or copy ctors
-#TODO default, delete ctors, explicit
-#TODO inherited ctors
-#TODO typedefs
-#TODO ignore empty ctor
+    return mock_generator(
+        files = args[1:],
+        args = clang_args,
+        decl = options.decl,
+        path = options.path,
+        mock_file_hpp = config['mock_file_hpp'],
+        file_template_hpp = config['file_template_hpp'],
+        mock_file_cpp = config['mock_file_cpp'],
+        file_template_cpp = config['file_template_cpp']
+    ).generate()
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
 
