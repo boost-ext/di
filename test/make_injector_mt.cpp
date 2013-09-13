75df041bf22ab1109177c0b7a9c2a9923bbefc08
Krzysztof Jusiak <krzysztof@jusiak.net>
2013-09-13 16:35:48 +0200

    old make_injector

--- test/make_injector_mt.cpp
+++ test/make_injector_mt.cpp
@@ -14,7 +14,6 @@
 
 #include "data.hpp"
 
-#include <boost/units/detail/utility.hpp>
 namespace boost {
 namespace di {
 
@@ -23,40 +22,22 @@ BOOST_AUTO_TEST_CASE(make_injector_ctor) {
         c0if0
     >;
 
-    auto f = make_injector(
-            bind<int>::to(32)
-    );
-
-    using injector_c1 = injector<
-        per_request<
-            double
-          , float
-        >
-    >;
-
-    auto m = injector_c0();
-    injector<injector_c0, per_request<int>, injector_c1, decltype(f)> inj(m, f);
-    using inj_t = decltype(inj);
-
-    std::cout << "dupa: " << boost::units::detail::demangle(typeid(inj_t::deps::type).name()) << std::endl;
-
-    shared_ptr<c5> c5_ = inj.create<shared_ptr<c5>>();
-    //BOOST_CHECK_EQUAL(0, c5_->c2_->i);
-    BOOST_CHECK(0);
+    shared_ptr<c5> c5_ = injector_c0().create<shared_ptr<c5>>();
+    BOOST_CHECK_EQUAL(0, c5_->c2_->i);
 }
 
 BOOST_AUTO_TEST_CASE(make_injector_by_value) {
-    //using injector_c0 = injector<
-        //c0if0
-    //>;
+    using injector_c0 = injector<
+        c0if0
+    >;
 
-    //auto injector_c0_int_ = make_injector(
-        //injector_c0()
-      //, bind<int>::to(42)
-    //);
+    auto injector_c0_int_ = make_injector(
+        injector_c0()
+      , bind<int>::to(42)
+    );
 
-    //shared_ptr<c5> c5_ = injector_c0_int_.create<shared_ptr<c5>>();
-    //BOOST_CHECK_EQUAL(42, c5_->c2_->i);
+    shared_ptr<c5> c5_ = injector_c0_int_.create<shared_ptr<c5>>();
+    BOOST_CHECK_EQUAL(42, c5_->c2_->i);
 }
 
 } // namespace di
