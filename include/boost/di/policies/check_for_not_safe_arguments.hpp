7915bf88b6333608b9e9a50ee64af362fff8a3ca
Krzysztof Jusiak <krzysztof@jusiak.net>
2013-09-20 18:17:54 +0200

    policies: not safe arugments, more cases

--- include/boost/di/policies/check_for_not_safe_arguments.hpp
+++ include/boost/di/policies/check_for_not_safe_arguments.hpp
@@ -1,6 +1,14 @@
 /**
  * @code
  * struct c { c(int*); };
+ *
+ * @code
+ * singleton<int>
+ * struct c { c(int&); }; // not ok
+ *
+ * singleton<int>
+ * struct c { c(int&); }; // ok
+ * struct d { d(shared_ptr<int>); }; // ok
  * @endcode
  */
 
