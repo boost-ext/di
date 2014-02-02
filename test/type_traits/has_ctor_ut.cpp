da640a84325cc255d9de33dd1f79bcd37285cbcb
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-02-02 10:46:54 +0100

    ctor -> inject

---
+++ test/type_traits/has_ctor_ut.cpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2014 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "boost/di/type_traits/has_ctor.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+namespace boost {
+namespace di {
+namespace type_traits {
+
+BOOST_AUTO_TEST_CASE(basic) {
+}
+
+} // namespace type_traits
+} // namespace di
+} // namespace boost
+
