d36461e98f31048b35d1426bb27fd16d6a386031
Krzysztof Jusiak (Chris) <krzysztof.jusiak@king.com>
2014-03-05 14:20:44 +0000

    preprocessed generation fixed

--- tools/preprocessed.sh
+++ tools/preprocessed.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 dump_file() {
-
-
     tmp=`mktemp`
     rm -rf $tmp
     mkdir $tmp
@@ -26,7 +24,7 @@ dump_file() {
 	mkdir -p $tmp/`dirname $1`
 	cp $1 $tmp/$1
 
-	$CXX -E $1 -I$tmp -include $2 | grep -v "^#" | cat -s
+	$CXX -E $1 -CC -I$tmp -include $2 | grep -v "^#" | sed '/\/\*.*\*\// d; /\/\*/,/\*\// d' | grep -v "^/" | cat -s
 
     rm -rf $tmp
 }
@@ -56,56 +54,27 @@ guard_end() {
 includes() {
     cat /tmp/i.hpp | grep -v preprocess | sort -u -r
 }
-files="
-boost/di/type_traits/remove_accessors.hpp
-boost/di/type_traits/make_plain.hpp
-boost/di/type_traits/is_same_base_of.hpp
-boost/di/type_traits/parameter_types.hpp
-boost/di/type_traits/has_ctor.hpp
-boost/di/type_traits/ctor_traits.hpp
-boost/di/type_traits/create_traits.hpp
-boost/di/named.hpp
-boost/di/convertibles/shared.hpp
-boost/di/convertibles/copy.hpp
-boost/di/convertibles/value.hpp
-boost/di/convertibles/reference.hpp
-boost/di/scopes/shared.hpp
-boost/di/scopes/unique.hpp
-boost/di/scopes/deduce.hpp
-boost/di/scopes/external.hpp
-boost/di/scopes/session.hpp
-boost/di/type_traits/scope_traits.hpp
-boost/di/concepts/type_traits/is_req_type.hpp
-boost/di/concepts/type_traits/is_req_name.hpp
-boost/di/concepts/type_traits/is_req_call.hpp
-boost/di/concepts/type_traits/priority.hpp
-boost/di/concepts/bind.hpp
-boost/di/concepts/scope.hpp
-boost/di/concepts/dependency.hpp
-boost/di/detail/binder.hpp
-boost/di/policies/arguments_permission.hpp
-boost/di/policies/creation_ownership.hpp
-boost/di/policies/binding_correctness.hpp
-boost/di/policies/circular_dependencies.hpp
-boost/di/policies/scopes_permission.hpp
-boost/di/detail/pool.hpp
-boost/di/detail/creator.hpp
-boost/di/detail/module.hpp
-boost/di/concepts.hpp
-boost/di/inject.hpp
-boost/di/provider.hpp
-boost/di/injector.hpp
-boost/di/make_injector.hpp
-"
+
+genereate_files() {
+    cat $1 | grep "#include" | grep "boost\/di" | grep -v PP_ | grep -v "preprocess" | while read include; do
+		file=`echo $include | sed "s/[^<^\"]*[\"<]\([^>\"]*\)[\">].*/\1/"`
+		if [[ "$file" =~ "di/aux_" ]] ||
+		   [[ "$file" =~ "inject.hpp" ]] ||
+		   [[ "$file" =~ "di.hpp" ]]; then
+			continue;
+		fi
+
+        genereate_files $file
+        echo $file
+    done
+}
 
 generate_preprocessed() {
     rm -rf boost/di/aux_/preprocessed/di.hpp /tmp/i.hpp /tmp/f.hpp
     mkdir -p boost/di/aux_/preprocessed/
 
     guard_begin > boost/di/aux_/preprocessed/di.hpp
-    for file in $files; do
-        #new_file=`echo $file | sed "s/di\//$2\//g"`
-
+    for file in `genereate_files "boost/di.hpp" | cat -n | sort -uk2 | sort -nk1 | cut -f2-`; do
 		if [[ "$file" =~ "di/aux_" ]] ||
 		   [[ "$file" =~ "inject.hpp" ]] ||
 		   [[ "$file" =~ "di.hpp" ]]; then
@@ -113,7 +82,6 @@ generate_preprocessed() {
 		fi
 
 	    echo $file
-		#mkdir -p `dirname $new_file` 2>/dev/null
 		dump_file $file $3 $2 >> /tmp/f.hpp
     done
     includes >> boost/di/aux_/preprocessed/di.hpp
