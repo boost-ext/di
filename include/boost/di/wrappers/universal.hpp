c63b41314e3a5d4148c0c8ab3fe3e2c878668eb0
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-05-29 22:21:40 +0100

    universal refactor

--- include/boost/di/wrappers/universal.hpp
+++ include/boost/di/wrappers/universal.hpp
@@ -26,13 +26,58 @@ namespace wrappers {
 
 namespace detail {
 
+template<typename T, typename TSignature>
+class is_convertible
+{
+    template<typename U> static mpl::aux::yes_tag test(non_type<TSignature, &U::operator()>*);
+    template<typename>   static mpl::aux::no_tag test(...);
+
+public:
+    typedef is_convertible type;
+
+    BOOST_STATIC_CONSTANT(
+        bool
+      , value = sizeof(test<T>(0)) == sizeof(mpl::aux::yes_tag)
+    );
+};
+
+template<typename TValueType, typename T>
+struct is_convertible_to_ref
+    : mpl::or_<
+          is_convertible<TValueType, T&(TValueType::*)(const type<T&>&) const>
+        , is_convertible<TValueType, const T&(TValueType::*)(const type<const T&>&) const>
+      >
+{ };
+
+template<typename T>
+struct holder
+{
+    explicit holder(const T& value)
+        : held(value)
+    { }
+
+    T held;
+};
+
+template<typename T, typename TValueType>
+inline const T& callback_ref(const TValueType& value) {
+    return value(boost::type<const T&>());
+}
+
+template<typename TResult, typename T, typename TValueType>
+inline const TResult& callback_copy(std::vector<aux::shared_ptr<void> >& refs, const TValueType& value) {
+    aux::shared_ptr<holder<TResult> > object(new holder<TResult>(value(boost::type<T>())));
+    refs.push_back(object);
+    return object->held;
+}
+
 template<typename T>
 class universal_impl
 {
 public:
-    template<typename TObject>
-    explicit universal_impl(std::vector<aux::shared_ptr<void> >&, const TObject& object)
-        : callback_(boost::bind<T>(object, boost::type<T>()))
+    template<typename TValueType>
+    explicit universal_impl(std::vector<aux::shared_ptr<void> >&, const TValueType& value)
+        : callback_(boost::bind<T>(value, boost::type<T>()))
     { }
 
     operator T() const {
@@ -43,119 +88,13 @@ private:
     function<T()> callback_;
 };
 
-template<typename T>
-class universal_impl<const T&>
-{
-    template<typename TValueType>
-    struct holder
-    {
-        explicit holder(const TValueType& value)
-            : held(value)
-        { }
-
-        TValueType held;
-    };
-
-    template<typename TValueType, typename TSignature>
-    class is_convertible
-    {
-        template<typename U> static mpl::aux::yes_tag test(non_type<TSignature, &U::operator()>*);
-        template<typename>   static mpl::aux::no_tag test(...);
-
-    public:
-        typedef is_convertible type;
-
-        BOOST_STATIC_CONSTANT(
-            bool
-          , value = sizeof(test<TValueType>(0)) == sizeof(mpl::aux::yes_tag)
-        );
-    };
-
-    template<typename TObject, typename TValueType>
-    struct is_convertible_to_ref
-        : mpl::or_<
-              is_convertible<TObject, TValueType&(TObject::*)(const type<TValueType&>&) const>
-            , is_convertible<TObject, const TValueType&(TObject::*)(const type<const TValueType&>&) const>
-          >
-    { };
-
-    template<typename TObject>
-    static const T& callback_ref(const TObject& object) {
-        return object(boost::type<const T&>());
-    }
-
-    template<typename TObject>
-    static const T& callback_copy(std::vector<aux::shared_ptr<void> >& refs, const TObject& object) {
-        aux::shared_ptr<holder<T> > value(new holder<T>(object(boost::type<T>())));
-        refs.push_back(value);
-        return value->held;
-    }
-
-public:
-    template<typename TObject>
-    universal_impl(std::vector<aux::shared_ptr<void> >&
-                 , const TObject& object
-                 , typename enable_if<is_convertible_to_ref<TObject, T> >::type* = 0)
-        : callback_(boost::bind(&universal_impl<const T&>::callback_ref<TObject>, object))
-    { }
-
-    template<typename TObject>
-    universal_impl(std::vector<aux::shared_ptr<void> >& refs
-                 , const TObject& object
-                 , typename disable_if<is_convertible_to_ref<TObject, T> >::type* = 0)
-        : callback_(boost::bind(&universal_impl<const T&>::callback_copy<TObject>, boost::ref(refs), object))
-    { }
-
-    operator const T&() const {
-        return callback_();
-    }
-
-private:
-    function<const T&()> callback_;
-};
-
-template<typename T, typename TName>
-class universal_impl<const named<T, TName>&>
-{
-    template<typename TValueType>
-    struct holder
-    {
-        explicit holder(const TValueType& value)
-            : held(value)
-        { }
-
-        TValueType held;
-    };
-
-    template<typename TObject>
-    static const named<T, TName>& callback_copy(std::vector<aux::shared_ptr<void> >& refs, const TObject& object) {
-        aux::shared_ptr<holder<named<T, TName> > > value(new holder<named<T, TName> >(named<T, TName>(object(boost::type<T>()))));
-        refs.push_back(value);
-        return value->held;
-    }
-
-public:
-    template<typename TObject>
-    universal_impl(std::vector<aux::shared_ptr<void> >& refs
-                 , const TObject& object)
-        : callback_(boost::bind(&universal_impl<const named<T, TName>&>::callback_copy<TObject>, boost::ref(refs), object))
-    { }
-
-    operator const named<T, TName>&() const {
-        return callback_();
-    }
-
-private:
-    function<const named<T, TName>&()> callback_;
-};
-
 template<typename T, typename TName>
 class universal_impl<named<T, TName> >
 {
 public:
-    template<typename TObject>
-    universal_impl(std::vector<aux::shared_ptr<void> >&, const TObject& object)
-        : callback_(boost::bind<T>(object, boost::type<T>()))
+    template<typename TValueType>
+    universal_impl(std::vector<aux::shared_ptr<void> >&, const TValueType& value)
+        : callback_(boost::bind<T>(value, boost::type<T>()))
     { }
 
     operator T() const {
@@ -170,15 +109,59 @@ private:
     function<T()> callback_;
 };
 
+template<typename T>
+class universal_impl<const T&>
+{
+public:
+    template<typename TValueType>
+    universal_impl(std::vector<aux::shared_ptr<void> >&
+                 , const TValueType& value
+                 , typename enable_if<is_convertible_to_ref<TValueType, T> >::type* = 0)
+        : callback_(boost::bind(&callback_ref<T, TValueType>, value))
+    { }
+
+    template<typename TValueType>
+    universal_impl(std::vector<aux::shared_ptr<void> >& refs
+                 , const TValueType& value
+                 , typename disable_if<is_convertible_to_ref<TValueType, T> >::type* = 0)
+        : callback_(boost::bind(&callback_copy<T, T, TValueType>, boost::ref(refs), value))
+    { }
+
+    operator const T&() const {
+        return callback_();
+    }
+
+private:
+    function<const T&()> callback_;
+};
+
+template<typename T, typename TName>
+class universal_impl<const named<T, TName>&>
+{
+public:
+    template<typename TValueType>
+    universal_impl(std::vector<aux::shared_ptr<void> >& refs
+                 , const TValueType& value)
+        : callback_(boost::bind(&callback_copy<named<T, TName>, T, TValueType>, boost::ref(refs), value))
+    { }
+
+    operator const named<T, TName>&() const {
+        return callback_();
+    }
+
+private:
+    function<const named<T, TName>&()> callback_;
+};
+
 } // namespace detail
 
 template<typename T>
 class universal : public detail::universal_impl<T>
 {
 public:
-    template<typename TObject>
-    universal(std::vector<aux::shared_ptr<void> >& refs, const TObject& object)
-        : detail::universal_impl<T>(refs, object)
+    template<typename TValueType>
+    universal(std::vector<aux::shared_ptr<void> >& refs, const TValueType& value)
+        : detail::universal_impl<T>(refs, value)
     { }
 };
 
