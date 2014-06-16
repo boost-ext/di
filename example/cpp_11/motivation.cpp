866945bf174b8b738ae8ea416db0c8d5ad46b285
Krzysztof Jusiak (Chris) <krzysztof.jusiak@king.com>
2014-06-16 12:27:06 +0100

    more doc

---
+++ example/cpp_11/motivation.cpp
@@ -0,0 +1,39 @@
+//
+// Copyright (c) 2014 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+//[example
+#include <memory>
+
+/*<<include di headers>>*/
+#include <boost/di.hpp>
+
+/*<<Define class as usual>>*/
+class app {
+public:
+    app(const std::shared_ptr<int>&
+      , boost::shared_ptr<int>
+      , std::unique_ptr<int>
+      , double
+      , int
+      , const std::string&)
+    { }
+};
+
+int main() {
+    /*<<make injector>>*/
+    auto injector = boost::di::make_injector();
+
+    /*<<create app>>*/
+    app = injector.create<app>();
+
+    /*<<use app>>*/
+    //...
+
+    return 0;
+}
+//]
+
