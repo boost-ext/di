d6331fe10e46af3f6d8c79e31addc13f18867f7b
Krzysztof Jusiak (Chris) <krzysztof.jusiak@king.com>
2014-06-20 14:38:44 +0100

    cpp14 configuration example

--- example/cpp_14/configuration.cpp
+++ example/cpp_14/configuration.cpp
@@ -8,38 +8,67 @@
 //[configuration_cpp_14
 //`[h6 C++ 14]
 //<-
+#include <tuple>
 //->
+
 #include <boost/di.hpp>
 
 namespace di = boost::di;
 
-class module1 {
-public:
-    auto configure() {
-        return di::make_injector(
-            di::bind<i, impl>()
-          , di::bind<int>::to(42)
-        );
-    }
+//<-
+namespace {
+
+template<typename F, typename T, std::size_t... N>
+auto invoke_impl(const F& f, const T& t, const std::index_sequence<N...>&) {
+	return f(std::get<N>(t)...);
+}
+
+template<typename F, typename T>
+auto invoke(const F& f, const T& t) {
+	return invoke_impl(f, t, std::make_index_sequence<std::tuple_size<T>::value>());
+}
+
+} // namespace
+
+//->
+
+struct i { virtual ~i() { } };
+struct impl : i { };
+
+struct app {
+	explicit app(std::unique_ptr<i> up, int i) {
+        assert(dynamic_cast<impl*>(up.get()));
+		assert(i == 42);
+	}
 };
 
-class module2 {
-public:
-    auto configure() {
-        return di::make_injector(
-            di::bind<i2, impl2>()
+struct module1 {
+	auto configure() const {
+		return di::make_injector(
+            di::bind<i, impl>()
         );
-    }
+	}
+};
+
+struct module2 {
+	auto configure() const {
+		return di::make_injector(
+            di::bind_int<42>()
+        );
+	}
 };
 
 int main() {
-    auto injector = di::make_injector(
-        std::make_tuple(module1(), module2())
-    );
+	auto injector =
+		invoke([](const auto&... args) {
+			return di::make_injector(args.configure()...);
+		}
+	  , std::make_tuple(module1(), module2())
+	);
 
-    injector.create<app>().start();
+	injector.create<app>();
 
-    return 0;
+	return 0;
 }
 
 //`full code example: [@example/cpp_14/configuration.cpp configuration.cpp]
