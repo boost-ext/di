52a77b5082d85078d0b9bb41cd6909202b804b0c
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-06-28 07:49:29 +0100

    factory example with modules

--- example/cpp_14/factory.cpp
+++ example/cpp_14/factory.cpp
@@ -5,9 +5,13 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-//[factory_cpp_11
-//`[h6 C++ 11]
+//[factory_cpp_14
+//`[h6 C++ 14]
 //<-
+#if (__cplusplus < 201300L)
+    int main() { return 0; }
+#else
+
 #include <cassert>
 #include <memory>
 //->
@@ -18,45 +22,40 @@ namespace mpl = boost::mpl;
 
 namespace {
 
-struct i
-{
+enum eid { e1 = 1, e2 = 2 };
+
+struct i {
     virtual ~i() { };
     virtual void dummy() = 0;
 };
 
-struct impl1 : i
-{
-    impl1(int i) {
-        assert(i == 1);
+struct impl1 : i {
+    explicit impl1(const eid& id) {
+        assert(id == e1);
     }
 
-    void dummy() override {}
+    void dummy() override { }
 };
 
-struct impl2 : i
-{
-    impl2(int i) {
-        assert(i == 2);
+struct impl2 : i {
+    explicit impl2(const eid& id) {
+        assert(id == e2);
     }
 
     void dummy() override {}
 };
 
 template<typename T>
-struct c
-{
+struct c {
     c(std::shared_ptr<i> sp) {
         assert(sp.get());
         assert(dynamic_cast<T*>(sp.get()));
     }
 };
 
-enum eid { e1 = 1, e2 = 2 };
-
 } // namespace
 
-class config
-{
+class config {
     config& operator=(const config&);
 
 public:
@@ -64,30 +63,24 @@ public:
         : id(id)
     { }
 
-    template<typename T>
-    T create() const {
+    auto configure() const {
         auto common = di::make_injector(
-            di::bind<int>::to(static_cast<int>(id))
+            di::bind<eid>::to(id)
         );
 
-        auto injector = di::make_injector(
+        return di::make_injector(
             common
           , di::bind<i>::to(
                 [&]() -> std::shared_ptr<i> {
                     switch(id) {
-                        case e1:
-                            return common.create<std::shared_ptr<impl1>>();
-
-                        case e2:
-                            return common.create<std::shared_ptr<impl2>>();
+                        case e1: return common.create<std::shared_ptr<impl1>>();
+                        case e2: return common.create<std::shared_ptr<impl2>>();
                     }
 
                     return nullptr;
                 }
             )
         );
-
-        return injector.template create<T>();
     }
 
 private:
@@ -98,15 +91,18 @@ int main() {
     auto id = e1;
 
     config conf((id));
-    conf.create<c<impl1>>();
+    auto injector = di::make_injector(conf);
+    injector.create<c<impl1>>();
 
     id = e2;
-    conf.create<c<impl2>>();
+    injector.create<c<impl2>>();
     (void)id;
 
     return 0;
 }
 
-//`full code example: [@example/cpp_11/factory.cpp factory.cpp]
+//`full code example: [@example/cpp_14/factory.cpp factory.cpp]
 //]
+
+#endif
 
