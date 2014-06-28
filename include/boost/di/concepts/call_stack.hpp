a07ca3be61b8c25046f5cd271cc49508199af819
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-06-28 17:04:40 +0100

    call_stack ut

--- include/boost/di/concepts/call_stack.hpp
+++ include/boost/di/concepts/call_stack.hpp
@@ -10,9 +10,11 @@
 #include "boost/di/aux_/config.hpp"
 #include "boost/di/type_traits/make_plain.hpp"
 
+#include <boost/mpl/vector.hpp>
 #include <boost/mpl/size.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/mpl/max.hpp>
+#include <boost/mpl/empty.hpp>
 #include <boost/mpl/begin_end.hpp>
 #include <boost/mpl/iterator_range.hpp>
 #include <boost/mpl/advance.hpp>
