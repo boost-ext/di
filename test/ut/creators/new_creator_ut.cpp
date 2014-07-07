4358ca4f3a953a7e9b62656777fb4140104a487d
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-07-07 12:54:15 +0100

    new creator ut

--- test/ut/creators/new_creator_ut.cpp
+++ test/ut/creators/new_creator_ut.cpp
@@ -4,7 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#include "boost/di/type_traits/create_traits.hpp"
+#include "boost/di/creators/new_creator.hpp"
 
 #include <memory>
 #include <boost/test/unit_test.hpp>
@@ -18,7 +18,7 @@
 
 namespace boost {
 namespace di {
-namespace type_traits {
+namespace creators {
 
 struct empty { };
 
@@ -31,28 +31,28 @@ struct ctor
 };
 
 BOOST_AUTO_TEST_CASE(create_empty) {
-    aux::unique_ptr<empty> empty_(create_traits<empty, empty>());
+    aux::unique_ptr<empty> empty_(new_creator().create<empty, empty>());
     BOOST_CHECK(empty_.get());
 }
 
 BOOST_AUTO_TEST_CASE(create_ctor) {
     fake_wrapper<int> i(42);
     fake_wrapper<double> d(42.0);
-    aux::unique_ptr<ctor> ctor_(create_traits<ctor, ctor, decltype(i), decltype(d)>(i, d));
+    aux::unique_ptr<ctor> ctor_(new_creator().create<ctor, ctor, decltype(i), decltype(d)>(i, d));
     BOOST_CHECK(ctor_.get());
 }
 
 BOOST_AUTO_TEST_CASE(create_int_value) {
-    aux::unique_ptr<int> i(create_traits<int, mpl::int_<42>>());
+    aux::unique_ptr<int> i(new_creator().create<int, mpl::int_<42>>());
     BOOST_CHECK_EQUAL(42, *i);
 }
 
 BOOST_AUTO_TEST_CASE(create_string_value) {
-    aux::unique_ptr<std::string> s(create_traits<std::string, mpl::string<'s'>>());
+    aux::unique_ptr<std::string> s(new_creator().create<std::string, mpl::string<'s'>>());
     BOOST_CHECK_EQUAL("s", *s);
 }
 
-} // namespace type_traits
+} // namespace creators
 } // namespace di
 } // namespace boost
 
