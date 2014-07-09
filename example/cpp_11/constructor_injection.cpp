ee4033d2fe68d733720b53216d27269e22ba7b8e
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-07-09 17:59:51 +0100

    examples -> constructor injection

--- example/cpp_11/constructor_injection.cpp
+++ example/cpp_11/constructor_injection.cpp
@@ -5,7 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-//[inject_cpp_11
+//[constructor_injection
 //````C++11```
 //<-
 #include <cassert>
--- example/hello_world.cpp
+++ example/hello_world.cpp
@@ -53,4 +53,5 @@ int main() {
     return 0;
 }
 
+//]
 
