70aa360f9355c1475272af2f43df570e2bf249fb
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-08-18 17:31:01 +0100

    when_ut

--- test/ut/bindings/detail/when_ut.cpp
+++ test/ut/bindings/detail/when_ut.cpp
@@ -8,6 +8,7 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "boost/di/aux_/mpl.hpp"
 #include "common/fakes/fake_data.hpp"
 #include "common/fakes/fake_concept.hpp"
 #include "common/fakes/fake_scope.hpp"
@@ -18,39 +19,29 @@ namespace bindings {
 namespace detail {
 
 BOOST_AUTO_TEST_CASE(empty_context) {
-    BOOST_CHECK_EQUAL(1, (when_<aux::mpl::vector<>>::apply<fake_data<void, aux::mpl::vector<>, fake_scope<>>>::type::value));
+    BOOST_CHECK_EQUAL(1, (when_<>::apply<fake_data<void, type_list<>, fake_scope<>>>::type::value));
 }
 
 BOOST_AUTO_TEST_CASE(one_concept_fail) {
     BOOST_CHECK_EQUAL(0, (
-        when_<
-            aux::mpl::vector<
-                fake_concept<0>
-            >
-        >::apply<fake_data<void, aux::mpl::vector<>, fake_scope<>>>::type::value)
+        when_<fake_concept<0>>::apply<fake_data<void, type_list<>, fake_scope<>>>::type::value)
     );
 }
 
 BOOST_AUTO_TEST_CASE(one_concept_pass) {
     BOOST_CHECK_EQUAL(1, (
-        when_<
-            aux::mpl::vector<
-                fake_concept<1>
-            >
-        >::apply<fake_data<void, aux::mpl::vector<>, fake_scope<>>>::type::value)
+        when_<fake_concept<1>>::apply<fake_data<void, type_list<>, fake_scope<>>>::type::value)
     );
 }
 
 BOOST_AUTO_TEST_CASE(many_bindings_max_element) {
     BOOST_CHECK_EQUAL(8, (
         when_<
-            aux::mpl::vector<
-                fake_concept<5>
-              , fake_concept<8>
-              , fake_concept<1>
-              , fake_concept<3>
-            >
-        >::apply<fake_data<void, aux::mpl::vector<>, fake_scope<>>>::type::value)
+            fake_concept<5>
+          , fake_concept<8>
+          , fake_concept<1>
+          , fake_concept<3>
+        >::apply<fake_data<void, type_list<>, fake_scope<>>>::type::value)
     );
 }
 
