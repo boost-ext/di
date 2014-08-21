b6960c784d6f194636bce883f7a4e5c653efb451
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-08-21 15:55:16 +0100

    dummy restrict_types policy

--- include/boost/di/policies/restrict_types.hpp
+++ include/boost/di/policies/restrict_types.hpp
@@ -7,7 +7,9 @@
 #ifndef BOOST_DI_POLICIES_PERMISSIONS_HPP
 #define BOOST_DI_POLICIES_PERMISSIONS_HPP
 
-#include "boost/di/aux_/config.hpp"
+#include "boost/di/aux_/mpl.hpp"
+#include "boost/di/type_traits/remove_accessors.hpp"
+#include "boost/di/scopes/deduce.hpp"
 
 namespace boost {
 namespace di {
@@ -23,74 +25,58 @@ struct allow_smart_ptrs {
 
 struct allow_refs {
     template<typename T>
-    using allow = and_<
-        !std::is_const<typename std::remove_reference<T>::type>::value
-        std::is_reference<T>::value
+    using allow = bool_<
+        std::is_reference<T>::value &&
+        !std::is_const<typename std::remove_reference<T>::type>::value &&
         !has_element_type<typename type_traits::remove_accessors<T>::type>::value
     >;
 };
 
 struct allow_const_refs {
     template<typename T>
-    struct allow
-        : aux::mpl::and_<
-              is_const<typename remove_reference<T>::type>
-            , is_reference<T>
-            , aux::mpl::not_<
-                  has_element_type<typename type_traits::remove_accessors<T>::type>
-              >
-          >
-    { };
+    using allow = bool_<
+        std::is_const<typename std::remove_reference<T>::type>::value &&
+        std::is_reference<T>::value &&
+        !has_element_type<typename type_traits::remove_accessors<T>::type>::value
+    >;
 };
 
 struct allow_rvalue_refs {
     template<typename T>
-    struct allow
-        : is_rvalue_reference<T>
-    { };
+    using allow = std::is_rvalue_reference<T>;
 };
 
 struct allow_ptrs {
     template<typename T>
-    struct allow
-        : is_pointer<T>
-    { };
+    using allow = std::is_pointer<T>;
 };
 
 struct allow_copies {
     template<typename T>
-    struct allow
-        : aux::mpl::and_<
-               aux::mpl::not_<is_reference<T>>
-             , aux::mpl::not_<is_pointer<T>>
-             , aux::mpl::not_<is_rvalue_reference<T>>
-             , aux::mpl::not_<has_element_type<typename type_traits::remove_accessors<T>::type>>
-          >
-    { };
+    using allow = bool_<
+        !std::is_reference<T>::value &&
+        !std::is_pointer<T>::value &&
+        !std::is_rvalue_reference<T>::value &&
+        !has_element_type<typename type_traits::remove_accessors<T>::type>::value
+    >;
 };
 
 template<typename TValueType>
 struct allow_type {
     template<typename T>
-    struct allow
-        : is_same<T, TValueType>
-    { };
+    using allow = std::is_same<T, TValueType>;
 };
 
 template<typename TExpr>
 struct allow_type_expr {
-    template<typename T>
-    struct allow
-        : aux::mpl::apply<TExpr, T>::type
-    { };
+    //template<typename T>
+    //using allow = ::mpl::apply<TExpr, T>::type;
 };
 
 template<typename TScope>
 struct allow_scope {
     template<typename T>
-    struct allow
-        : is_same<T, TScope>
-    { };
+    using allow = std::is_same<T, TScope>;
 };
 
 template<>
@@ -99,46 +85,46 @@ struct allow_scope<scopes::deduce>; // disabled
 template<typename... Ts>
 class restrict_types
 {
-    struct not_resolved {
-        typedef not_resolved type;
+/*    struct not_resolved {*/
+        //typedef not_resolved type;
 
-        template<typename>
-        struct rebind {
-            typedef not_resolved other;
-        };
-    };
+        //template<typename>
+        //struct rebind {
+            //typedef not_resolved other;
+        //};
+    //};
 
-    template<typename TAllow, typename T>
-    struct is_type_permitted_impl
-        : TAllow::template allow<T>
-    { };
+    //template<typename TAllow, typename T>
+    //struct is_type_permitted_impl
+        //: TAllow::template allow<T>
+    //{ };
 
-    template<typename T>
-    struct is_type_permitted
-        : aux::mpl::or_<
-              aux::mpl::not_<
-                  is_same<
-                      typename T::binder::template resolve<
-                          typename T::type
-                        , aux::mpl::vector<>
-                        , not_resolved
-                      >::type
-                    , not_resolved
-                  >
-              >
-            , aux::mpl::bool_<
-                  aux::mpl::count_if<
-                      permitted_types
-                    , is_type_permitted_impl<aux::mpl::_, typename T::type>
-                  >::value != 0
-              >
-          >
-    { };
+    //template<typename T>
+    //struct is_type_permitted
+        //: aux::mpl::or_<
+              //aux::mpl::not_<
+                  //is_same<
+                      //typename T::binder::template resolve<
+                          //typename T::type
+                        //, aux::mpl::vector<>
+                        //, not_resolved
+                      //>::type
+                    //, not_resolved
+                  //>
+              //>
+            //, aux::mpl::bool_<
+                  //aux::mpl::count_if<
+                      //permitted_types
+                    //, is_type_permitted_impl<aux::mpl::_, typename T::type>
+                  //>::value != 0
+              //>
+          //>
+    /*{ };*/
 
 public:
     template<typename T>
     void assert_policy() const {
-        static_assert(is_type_restricted<T>::value, "Type is not allowed");
+        //static_assert(is_type_restricted<T>::value, "Type is not allowed");
     }
 };
 
