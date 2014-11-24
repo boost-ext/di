1c3ac4bdc64bcb5cf0c2d52f7f344d7b0026545a
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-11-24 16:39:23 +0000

    core injector refactor

--- include/boost/di/core/injector.hpp
+++ include/boost/di/core/injector.hpp
@@ -18,6 +18,7 @@
 #include "boost/di/providers/nothrow.hpp"
 #include "boost/di/wrappers/universal.hpp"
 #include "boost/di/type_traits/ctor_traits.hpp"
+#include "boost/di/scopes/deduce.hpp"
 
 namespace boost {
 namespace di {
@@ -26,6 +27,51 @@ namespace core {
 BOOST_MPL_HAS_XXX_TRAIT_DEF(any)
 BOOST_MPL_HAS_XXX_TRAIT_DEF(create2)
 BOOST_MPL_HAS_XXX_TRAIT_DEF(create3)
+BOOST_MPL_HAS_XXX_TRAIT_DEF(deps)
+
+template<typename T>
+using is_injector = bool_<has_deps<T>::value || aux::has_configure<T>::value>;
+
+template<typename T, typename = void>
+struct get_injector {
+    using type = T;
+};
+
+template<typename T>
+struct get_injector<T, aux::enable_if_t<aux::has_configure<T>{}>> {
+    using type = typename aux::function_traits<decltype(&T::configure)>::result_type;
+};
+
+template<typename T, typename = void>
+struct get_deps {
+    using type = typename T::deps;
+};
+
+template<typename T>
+struct get_deps<T, aux::enable_if_t<aux::has_configure<T>{}>> {
+    using type = typename get_injector<T>::type::deps;
+};
+
+template<typename T, typename = typename is_type_list<T>::type, typename = typename is_injector<T>::type>
+struct add_type_list;
+
+template<typename T, typename TAny>
+struct add_type_list<T, std::true_type, TAny> {
+    using type = T;
+};
+
+template<typename T>
+struct add_type_list<T, std::false_type, std::true_type> {
+    using type = typename get_deps<T>::type;
+};
+
+template<typename T>
+struct add_type_list<T, std::false_type, std::false_type> {
+    using type = type_list<T>; //scope<scopes::deduce>::bind<T>;
+};
+
+template<typename... Ts>
+using bindings_t = typename join<typename add_type_list<Ts>::type...>::type;
 
 template<typename TDeps = type_list<>, typename TDefaultProvider = providers::nothrow>
 class injector : public pool<TDeps> {
@@ -57,7 +103,7 @@ public:
 
     template<typename... TArgs>
     injector(const TArgs&... args)
-        : pool_t(pool<type_list<TArgs...>>(args...), init{})
+        : pool_t(pool<type_list<TArgs...>>(pass_arg(args)...), init{})
     { }
 
     template<typename T, typename... TArgs>
@@ -242,6 +288,18 @@ private:
 
     template<typename T, typename TDeps_, typename TAction>
     aux::enable_if_t<!aux::has_call<T>{}> call_impl(TDeps_&, const TAction&) { }
+
+    template<typename T>
+    aux::enable_if_t<!aux::has_configure<T>::value, const T&>
+    pass_arg(const T& arg) const {
+        return arg;
+    }
+
+    template<typename T>
+    aux::enable_if_t<aux::has_configure<T>::value, typename get_injector<T>::type>
+    pass_arg(const T& arg) const {
+        return arg.configure();
+    }
 };
 
 } // namespace core
--- include/boost/di/injector.hpp
+++ include/boost/di/injector.hpp
@@ -7,86 +7,13 @@
 #ifndef BOOST_DI_INJECTOR_HPP
 #define BOOST_DI_INJECTOR_HPP
 
-#include "boost/di/aux_/type_traits.hpp"
-#include "boost/di/scopes/deduce.hpp"
 #include "boost/di/core/injector.hpp"
 
 namespace boost {
 namespace di {
 
-namespace detail {
-
-BOOST_MPL_HAS_XXX_TRAIT_DEF(deps)
-
-template<typename T>
-using is_injector = bool_<has_deps<T>::value || aux::has_configure<T>::value>;
-
-template<typename T, typename = void>
-struct get_injector {
-    using type = T;
-};
-
-template<typename T>
-struct get_injector<T, aux::enable_if_t<aux::has_configure<T>{}>> {
-    using type = typename aux::function_traits<decltype(&T::configure)>::result_type;
-};
-
-template<typename T, typename = void>
-struct get_deps {
-    using type = typename T::deps;
-};
-
-template<typename T>
-struct get_deps<T, aux::enable_if_t<aux::has_configure<T>{}>> {
-    using type = typename get_injector<T>::type::deps;
-};
-
-template<typename T, typename = typename is_type_list<T>::type, typename = typename is_injector<T>::type>
-struct add_type_list;
-
-template<typename T, typename TAny>
-struct add_type_list<T, std::true_type, TAny> {
-    using type = T;
-};
-
-template<typename T>
-struct add_type_list<T, std::false_type, std::true_type> {
-    using type = typename get_deps<T>::type;
-};
-
-template<typename T>
-struct add_type_list<T, std::false_type, std::false_type> {
-    using type = type_list<T>; //scope<scopes::deduce>::bind<T>;
-};
-
 template<typename... Ts>
-using bindings_t = typename join<typename add_type_list<Ts>::type...>::type;
-
-} // namespace detail
-
-template<typename... Ts>
-class injector
-    : public core::injector<detail::bindings_t<Ts...>>
-{
-public:
-    template<typename... TArgs>
-    injector(const TArgs&... args)
-        : core::injector<detail::bindings_t<Ts...>>(pass_arg(args)...)
-    { }
-
-private:
-    template<typename T>
-    aux::enable_if_t<!aux::has_configure<T>::value, const T&>
-    pass_arg(const T& arg) const {
-        return arg;
-    }
-
-    template<typename T>
-    aux::enable_if_t<aux::has_configure<T>::value, typename detail::get_injector<T>::type>
-    pass_arg(const T& arg) const {
-        return arg.configure();
-    }
-};
+using injector = core::injector<core::bindings_t<Ts...>>;
 
 } // namespace di
 } // namespace boost
