f7da9954f174c6a39ef5f3df0003b15d2f3ab55a
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-12-15 17:48:15 +0000

    allow_ctor_types

--- test/common/fakes/fake_data.hpp
+++ test/common/fakes/fake_data.hpp
@@ -7,19 +7,46 @@
 #ifndef BOOST_DI_FAKE_DATA_HPP
 #define BOOST_DI_FAKE_DATA_HPP
 
+#include "boost/di/aux_/utility.hpp"
+
 namespace boost { namespace di {
 
-template<class T, class TDependency>
-struct fake_data {
+template<
+    class T
+  , class TDependency = aux::none_t
+  , class TDeps = aux::none_t
+  , bool TResolve = true
+> struct fake_data;
+
+template<
+    class T
+  , class TDependency
+  , class TDeps
+> struct fake_data<T, TDependency, TDeps, true> {
     using type = T;
 
-    TDependency&& dep;
-    //pool_t& deps;
+    TDependency dep;
+    TDeps deps;
 
-    template<class U, class TDefault>
+    template<class, class TDefault>
     decltype(auto) resolve() const noexcept {
-        //return binder::template resolve<U, TDefault>(&deps);
-        return 0;
+        return TDependency{};
+    }
+};
+
+template<
+    class T
+  , class TDependency
+  , class TDeps
+> struct fake_data<T, TDependency, TDeps, false> {
+    using type = T;
+
+    TDependency dep;
+    TDeps deps;
+
+    template<class, class TDefault>
+    decltype(auto) resolve() const noexcept {
+        return TDefault{};
     }
 };
 
--- test/ut/policies/allow_ctor_types.cpp
+++ test/ut/policies/allow_ctor_types.cpp
@@ -6,14 +6,61 @@
 //
 #include "common/fakes/fake_assert.hpp"
 #include "common/fakes/fake_data.hpp"
+#include <type_traits>
 #include "boost/di/policies/allow_ctor_types.hpp"
 
 namespace boost { namespace di { namespace policies {
 
-test type_not_allowed = [] {
-    try { allow_ctor_types()(fake_data<int, int>{int{}}); } catch(const assert_exception&) { };
+test nothing_is_allowed = [] {
+    struct c { };
+    try { allow_ctor_types()(fake_data<int>{}); } catch(const assert_exception&) { };
+    try { allow_ctor_types()(fake_data<double>{}); } catch(const assert_exception&) { };
+    try { allow_ctor_types()(fake_data<c>{}); } catch(const assert_exception&) { };
 };
 
+test type_is_allowed = [] {
+    auto test = [](auto type) {
+        allow_ctor_types(std::is_same<decltype(type), _>{})(fake_data<decltype(type)>{});
+    };
+
+    struct c { };
+    test(int{});
+    test(float{});
+    test(c{});
+};
+
+test type_is_not_allowed = [] {
+    auto test = [](auto type) {
+         try { allow_ctor_types(std::is_same<decltype(type), _>{})(fake_data<void>{}); } catch(const assert_exception&) { }
+    };
+
+    struct c { };
+    test(int{});
+    test(float{});
+    test(c{});
+};
+
+test type_is_allowed_operator_or = [] {
+    auto test = [](auto type1, auto type2, auto allowed) {
+        using namespace operators;
+        allow_ctor_types(std::is_same<decltype(type1), _>{} || std::is_same<_, decltype(type2)>{})(fake_data<decltype(allowed)>{});
+    };
+
+    test(int{}, double{}, int{});
+    test(double{}, int{}, int{});
+};
+
+test type_is_not_allowed_operator_or = [] {
+    auto test = [](auto type1, auto type2, auto allowed) {
+        using namespace operators;
+        try { allow_ctor_types(
+            std::is_same<decltype(type1), _>{} || std::is_same<_, decltype(type2)>{})(fake_data<decltype(allowed)>{});
+        } catch(const assert_exception&) { }
+    };
+
+    test(int{}, double{}, float{});
+    test(double{}, int{}, float{});
+};
 
 }}} // boost::di::policies
 
