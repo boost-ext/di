096d742b4afe9f5ffa1585b8bfe1a9bf074fc947
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-12-22 17:55:26 +0000

    better named example

--- example/named.cpp
+++ example/named.cpp
@@ -19,16 +19,17 @@ static struct int_2{} int_2;
 class named {
 public:
     /*<<Constructor with named parameters of the same `int` type>>*/
-    BOOST_DI_INJECT(named, (named = int_1) int i1, (named = int_2) int i2)
-        : i1(i1), i2(i2)
-    {
+    BOOST_DI_INJECT(named, (named = int_1) int i1, (named = int_2) int i2, int i3)
+        : i1(i1), i2(i2), i3(i3) {
         assert(i1 == 42);
         assert(i2 == 87);
+        assert(i3 == 123);
     }
 
 private:
     int i1 = 0;
     int i2 = 0;
+    int i3 = 0;
 };
 
 int main() {
@@ -36,6 +37,7 @@ int main() {
     auto injector = di::make_injector(
         di::bind<int>.named(int_1).to(42)
       , di::bind<int>.named(int_2).to(87)
+      , di::bind<int>.to(123)
     );
 
     /*<<create `named`>>*/
