c62b47900c9b43a274b460ddfd58d05de0c3a425
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-12-22 17:38:46 +0000

    more fail tests

---
+++ test/ft/error_bind_multiple_times.cpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2014 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "boost/di.hpp"
+
+namespace di = boost::di;
+
+struct i { };
+struct impl1 : i { };
+struct impl2 : i { };
+
+test error_bind_to_different_types = [] {
+    di::make_injector(
+        di::bind<i, impl1>
+      , di::bind<i, impl2>
+    );
+};
+
