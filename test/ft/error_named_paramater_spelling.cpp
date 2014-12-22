840377bc41d716f3f61d0d929a44382625ccfd8d
Krzysztof Jusiak <krzysztof@jusiak.net>
2014-12-22 20:53:12 +0000

    error_bind_external_with_given_scope

---
+++ test/ft/error_named_paramater_spelling.cpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2014 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "boost/di.hpp"
+
+namespace di = boost::di;
+
+struct name {} name;
+
+test error_bind_external_with_given_scope = [] {
+    struct c {
+        BOOST_DI_INJECT(c, (NAMED = name) int) { }
+    };
+};
+
