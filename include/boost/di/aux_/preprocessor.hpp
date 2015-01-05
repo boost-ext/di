1e3a94206035a4b218a6e019589a22535cb49049
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-01-05 15:52:52 +0000

    inject style

--- include/boost/di/aux_/preprocessor.hpp
+++ include/boost/di/aux_/preprocessor.hpp
@@ -7,53 +7,66 @@
 #ifndef BOOST_DI_AUX_PREPROCESSOR_HPP
 #define BOOST_DI_AUX_PREPROCESSOR_HPP
 
-#define BOOST_DI_NARG_(...) BOOST_DI_ARG_N(__VA_ARGS__)
-#define BOOST_DI_NARG(...) BOOST_DI_NARG_(__VA_ARGS__, RSEQ_N())
-#define BOOST_DI_COMMA() ,
-#define BOOST_DI_PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
 #define BOOST_DI_CAT(a, ...) BOOST_DI_PRIMITIVE_CAT(a, __VA_ARGS__)
 #define BOOST_DI_CALL(m, ...) m(__VA_ARGS__)
+#define BOOST_DI_EMPTY()
+#define BOOST_DI_COMMA() ,
+#define BOOST_DI_EAT(...)
+#define BOOST_DI_EXPAND(...) __VA_ARGS__
+#define BOOST_DI_NARG_(...) BOOST_DI_ARG_N(__VA_ARGS__)
+#define BOOST_DI_NARG(...) BOOST_DI_NARG_(__VA_ARGS__, BOOST_DI_RSEQ_N())
+#define BOOST_DI_PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
 #define BOOST_DI_IBP_SPLIT(i, ...) BOOST_DI_PRIMITIVE_CAT(BOOST_DI_IBP_SPLIT_, i)(__VA_ARGS__)
 #define BOOST_DI_IBP_SPLIT_0(a, ...) a
 #define BOOST_DI_IBP_SPLIT_1(a, ...) __VA_ARGS__
 #define BOOST_DI_IBP_IS_VARIADIC_C(...) 1
 #define BOOST_DI_IBP_IS_VARIADIC_R_1 1,
 #define BOOST_DI_IBP_IS_VARIADIC_R_BOOST_DI_IBP_IS_VARIADIC_C 0,
-#define BOOST_DI_IBP(...) BOOST_DI_IBP_SPLIT(0, BOOST_DI_CAT(BOOST_DI_IBP_IS_VARIADIC_R_, BOOST_DI_IBP_IS_VARIADIC_C __VA_ARGS__))
-#define BOOST_DI_EMPTY()
+#define BOOST_DI_IBP(...) \
+    BOOST_DI_IBP_SPLIT(0, BOOST_DI_CAT( \
+        BOOST_DI_IBP_IS_VARIADIC_R_, BOOST_DI_IBP_IS_VARIADIC_C __VA_ARGS__) \
+    )
 #define BOOST_DI_TRIGGER_PARENTHESIS(...) ,
-#define ISEMPTY(...) _ISEMPTY(HAS_COMMA(__VA_ARGS__), HAS_COMMA(BOOST_DI_TRIGGER_PARENTHESIS __VA_ARGS__), HAS_COMMA(__VA_ARGS__ ()), HAS_COMMA(BOOST_DI_TRIGGER_PARENTHESIS __VA_ARGS__ ()))
-#define DEFER(id) id BOOST_DI_EMPTY()
-#define OBSTRUCT(...) __VA_ARGS__ DEFER(BOOST_DI_EMPTY)()
-#define EVAL(...) EVAL1(EVAL1(EVAL1(__VA_ARGS__)))
-#define EVAL1(...) EVAL2(EVAL2(EVAL2(__VA_ARGS__)))
-#define EVAL2(...) __VA_ARGS__
-#define CHECK_N(x, n, ...) n
-#define CHECK(...) CHECK_N(__VA_ARGS__, 0,)
-#define NOT(x) CHECK(BOOST_DI_PRIMITIVE_CAT(NOT_, x))
-#define NOT_0 ~, 1,
-#define COMPL(b) BOOST_DI_PRIMITIVE_CAT(COMPL_, b)
-#define COMPL_0 1
-#define COMPL_1 0
-#define BOOL(x) COMPL(NOT(x))
-#define IIF(c) BOOST_DI_PRIMITIVE_CAT(IIF_, c)
-#define IIF_0(t, ...) __VA_ARGS__
-#define IIF_1(t, ...) t
-#define BOOST_DI_IF(c) IIF(BOOL(c))
-#define PASTE5(_0, _1, _2, _3, _4) _0 ## _1 ## _2 ## _3 ## _4
-#define _ISEMPTY(_0, _1, _2, _3) HAS_COMMA(PASTE5(_IS_EMPTY_CASE_, _0, _1, _2, _3))
-#define _IS_EMPTY_CASE_0001 ,
-#define EAT(...)
-#define EXPAND(...) __VA_ARGS__
-#define WHEN(c) BOOST_DI_IF(c)(EXPAND, EAT)
-#define REPEAT(count, macro, ...) WHEN(count)(OBSTRUCT(REPEAT_INDIRECT)()(BOOST_DI_DEC(count), macro, __VA_ARGS__) OBSTRUCT(macro) (BOOST_DI_DEC(count), __VA_ARGS__ ))
-#define REPEAT_INDIRECT() REPEAT
-#define BOOST_DI_VARARG_IMPL2(m, count, ...) m##count(__VA_ARGS__)
-#define BOOST_DI_VARARG_IMPL(m, count, ...) BOOST_DI_VARARG_IMPL2(m, count, __VA_ARGS__)
-
+#define BOOST_DI_IS_EMPTY(...)  \
+    BOOST_DI_IS_EMPTY_IMPL(BOOST_DI_HAS_COMMA(__VA_ARGS__) \
+           , BOOST_DI_HAS_COMMA(BOOST_DI_TRIGGER_PARENTHESIS __VA_ARGS__) \
+           , BOOST_DI_HAS_COMMA(__VA_ARGS__ ()) \
+           , BOOST_DI_HAS_COMMA(BOOST_DI_TRIGGER_PARENTHESIS __VA_ARGS__ ()))                     
+#define BOOST_DI_IS_EMPTY_IMPL(_0, _1, _2, _3) \
+    BOOST_DI_HAS_COMMA(BOOST_DI_JOIN_5(BOOST_DI_IS_EMPTY_IMPL_, _0, _1, _2, _3))
+#define BOOST_DI_DEFER(id) id BOOST_DI_EMPTY()
+#define BOOST_DI_OBSTRUCT(...) __VA_ARGS__ BOOST_DI_DEFER(BOOST_DI_EMPTY)()
+#define BOOST_DI_EVAL(...) BOOST_DI_EVAL1(BOOST_DI_EVAL1(BOOST_DI_EVAL1(__VA_ARGS__)))
+#define BOOST_DI_EVAL1(...) BOOST_DI_EVAL2(BOOST_DI_EVAL2(BOOST_DI_EVAL2(__VA_ARGS__)))
+#define BOOST_DI_EVAL2(...) __VA_ARGS__
+#define BOOST_DI_CHECK_N(x, n, ...) n
+#define BOOST_DI_CHECK(...) BOOST_DI_CHECK_N(__VA_ARGS__, 0,)
+#define BOOST_DI_NOT(x) BOOST_DI_CHECK(BOOST_DI_PRIMITIVE_CAT(BOOST_DI_NOT_, x))
+#define BOOST_DI_NOT_0 ~, 1,
+#define BOOST_DI_COMPL(b) BOOST_DI_PRIMITIVE_CAT(BOOST_DI_COMPL_, b)
+#define BOOST_DI_COMPL_0 1
+#define BOOST_DI_COMPL_1 0
+#define BOOST_DI_BOOL(x) BOOST_DI_COMPL(BOOST_DI_NOT(x))
+#define BOOST_DI_IF_IMPL(c) BOOST_DI_PRIMITIVE_CAT(BOOST_DI_IF_IMPL_, c)
+#define BOOST_DI_IF_IMPL_0(t, ...) __VA_ARGS__
+#define BOOST_DI_IF_IMPL_1(t, ...) t
+#define BOOST_DI_IF(c) BOOST_DI_IF_IMPL(BOOST_DI_BOOL(c))
+#define BOOST_DI_JOIN_5(_0, _1, _2, _3, _4) _0 ## _1 ## _2 ## _3 ## _4
+#define BOOST_DI_IS_EMPTY_IMPL_0001 ,
+#define BOOST_DI_REPEAT(n, m, ...) \
+    BOOST_DI_IF(n)(BOOST_DI_EXPAND, BOOST_DI_EAT)( \
+        BOOST_DI_OBSTRUCT(BOOST_DI_REPEAT_INDIRECT)()( \
+            BOOST_DI_DEC(n), m, __VA_ARGS__ \
+        ) \
+    BOOST_DI_OBSTRUCT(m)( \
+        BOOST_DI_DEC(n), __VA_ARGS__ ) \
+    )
+#define BOOST_DI_REPEAT_INDIRECT() BOOST_DI_REPEAT
+#define BOOST_DI_VARARG_IMPL2(m, n, ...) m##n(__VA_ARGS__)
+#define BOOST_DI_VARARG_IMPL(m, n, ...) BOOST_DI_VARARG_IMPL2(m, n, __VA_ARGS__)
 #define BOOST_DI_ARG_N( _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, N, ...) N
-#define RSEQ_N() 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
-#define HAS_COMMA(...) BOOST_DI_ARG_N(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)
+#define BOOST_DI_RSEQ_N() 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define BOOST_DI_HAS_COMMA(...) BOOST_DI_ARG_N(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)
 #define BOOST_DI_ARG0(p1, ...) p1
 #define BOOST_DI_ARG1(p1, p2, ...) p2
 #define BOOST_DI_ARG2(p1, p2, p3, ...) p3
--- include/boost/di/inject.hpp
+++ include/boost/di/inject.hpp
@@ -25,17 +25,17 @@
 #define BOOST_DI_GEN_NAME_ARG_END() );
 #define BOOST_DI_GEN_ARG(p) static void BOOST_DI_CAT(BOOST_DI_INJECTOR, arg)(p);
 #define BOOST_DI_GEN_CTOR(i, ...) BOOST_DI_GEN_CTOR_IMPL(BOOST_DI_VARARG_IMPL(BOOST_DI_ARG, i, __VA_ARGS__,), i)
-#define BOOST_DI_GEN_CTOR_IMPL(p, i) BOOST_DI_IF(i)(BOOST_DI_COMMA(),) BOOST_DI_IF(BOOST_DI_IBP(p))(EAT p, p)
+#define BOOST_DI_GEN_CTOR_IMPL(p, i) BOOST_DI_IF(i)(BOOST_DI_COMMA(),) BOOST_DI_IF(BOOST_DI_IBP(p))(BOOST_DI_EAT p, p)
 #define BOOST_DI_GEN_TYPE_LIST(i, ...) BOOST_DI_GEN_TYPE_LIST_IMPL(BOOST_DI_VARARG_IMPL(BOOST_DI_ARG, i, __VA_ARGS__,), i)
 #define BOOST_DI_GEN_TYPE_LIST_IMPL(p, n)  BOOST_DI_IF(n)(BOOST_DI_COMMA(),) BOOST_DI_IF(BOOST_DI_IBP(p))( \
     const ::boost::di::aux::type<arg##n BOOST_DI_COMMA() ::std::true_type>& \
-  , BOOST_DI_IF(ISEMPTY(p))(,const ::boost::di::aux::type<arg##n BOOST_DI_COMMA() ::std::false_type>&))
+  , BOOST_DI_IF(BOOST_DI_IS_EMPTY(p))(,const ::boost::di::aux::type<arg##n BOOST_DI_COMMA() ::std::false_type>&))
 
 #if !defined(BOOST_DI_INJECT_TRAITS)
     #define BOOST_DI_INJECT_TRAITS(...) \
         struct BOOST_DI_INJECTOR { \
-            EVAL(REPEAT(BOOST_DI_NARG(__VA_ARGS__), BOOST_DI_GEN_ARGS, __VA_ARGS__)) \
-            using type = ::boost::di::aux::type_list<EVAL(REPEAT(BOOST_DI_NARG(__VA_ARGS__), BOOST_DI_GEN_TYPE_LIST, __VA_ARGS__))>; \
+            BOOST_DI_EVAL(BOOST_DI_REPEAT(BOOST_DI_NARG(__VA_ARGS__), BOOST_DI_GEN_ARGS, __VA_ARGS__)) \
+            using type = ::boost::di::aux::type_list<BOOST_DI_EVAL(BOOST_DI_REPEAT(BOOST_DI_NARG(__VA_ARGS__), BOOST_DI_GEN_TYPE_LIST, __VA_ARGS__))>; \
             static_assert(BOOST_DI_NARG(__VA_ARGS__) <= BOOST_DI_CFG_CTOR_LIMIT_SIZE, "Number of constructor arguments is out of range - see BOOST_DI_CFG_CTOR_LIMIT_SIZE"); \
         }
 #endif
@@ -51,7 +51,7 @@
 #if !defined(BOOST_DI_INJECT)
     #define BOOST_DI_INJECT(type, ...) \
         BOOST_DI_INJECT_TRAITS(__VA_ARGS__); \
-        type(EVAL(REPEAT(BOOST_DI_NARG(__VA_ARGS__), BOOST_DI_GEN_CTOR, __VA_ARGS__)))
+        type(BOOST_DI_EVAL(BOOST_DI_REPEAT(BOOST_DI_NARG(__VA_ARGS__), BOOST_DI_GEN_CTOR, __VA_ARGS__)))
 #endif
 
 #endif
