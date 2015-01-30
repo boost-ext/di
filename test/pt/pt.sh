03ff23e52e48316d1b0f89d26f5b9d028f5527ab
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-01-30 20:31:52 +0000

    license info

--- test/pt/pt.sh
+++ test/pt/pt.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (c) 2014 Krzysztof Jusiak (krzysztof at jusiak dot net)
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
 
 bind_interfaces() {
     for ((i=1; i<=$1; ++i)); do
