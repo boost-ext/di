1c4855dd1172031e18d80ed99bde6c2f376b5688
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-04-13 15:42:03 +0100

    msvc2015 tests

--- test/msvc/msvc2015.cpp
+++ test/msvc/msvc2015.cpp
@@ -1,4 +1,11 @@
-#define BOOST_DI_CFG_NO_PREPROCESSED_HEADERS
+//
+// Copyright (c) 2014 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#if defined(_MSC_VER)
+
 #include <boost/di.hpp>
 #include <cassert>
 
@@ -70,4 +77,6 @@ int main() {
 
     return 0;
 }
+
+#endif
 
