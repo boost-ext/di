226fc53a0774ac5425526d8d35d35645678a1474
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-04-13 15:58:37 +0100

    jamfiles with msvc

--- include/boost/di.hpp
+++ include/boost/di.hpp
@@ -1052,7 +1052,6 @@ public:
 
 #define BOOST_DI_IF(cond, t, f) BOOST_DI_IF_I(cond, t, f)
 #define BOOST_DI_REPEAT(i, m, ...) BOOST_DI_REPEAT_N(i, m, __VA_ARGS__)
-#define BOOST_DI_EMPTY()
 #define BOOST_DI_CAT(a, ...) BOOST_DI_PRIMITIVE_CAT(a, __VA_ARGS__)
 #define BOOST_DI_CALL(m, ...) m(__VA_ARGS__)
 #define BOOST_DI_EMPTY()
@@ -1061,13 +1060,12 @@ public:
 #define BOOST_DI_EXPAND(...) __VA_ARGS__
 #define BOOST_DI_NARG(...) BOOST_DI_CAT(BOOST_DI_VARIADIC_SIZE_I(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,),)
 #define BOOST_DI_PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
-#define BOOST_DI_IS_EMPTY(...)  \
+#define BOOST_DI_ARG(n, ...) BOOST_DI_ARG_I(n,__VA_ARGS__)
+#define BOOST_DI_IS_EMPTY(...) \
     BOOST_DI_IS_EMPTY_IMPL(BOOST_DI_HAS_COMMA(__VA_ARGS__) \
            , BOOST_DI_HAS_COMMA(BOOST_DI_TRIGGER_PARENTHESIS __VA_ARGS__) \
            , BOOST_DI_HAS_COMMA(__VA_ARGS__ ()) \
            , BOOST_DI_HAS_COMMA(BOOST_DI_TRIGGER_PARENTHESIS __VA_ARGS__ ()))
-
-// detail
 
 #define BOOST_DI_VARIADIC_SIZE_I(e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, size, ...) size
 #define BOOST_DI_TRIGGER_PARENTHESIS(...) ,
@@ -1076,7 +1074,6 @@ public:
 #define BOOST_DI_IS_EMPTY_IMPL_0001 ,
 #define BOOST_DI_ARG_N( _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, N, ...) N
 #define BOOST_DI_HAS_COMMA(...) BOOST_DI_ARG_N(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)
-
 #define BOOST_DI_IF_I(cond, t, f) BOOST_DI_IIF(cond, t, f)
 #define BOOST_DI_IIF_0(t, f) f
 #define BOOST_DI_IIF_1(t, f) t
@@ -1088,8 +1085,6 @@ public:
 #define BOOST_DI_IIF_7(t, f) t
 #define BOOST_DI_IIF_8(t, f) t
 #define BOOST_DI_IIF_9(t, f) t
-
-#define BOOST_DI_ARG(n, ...) BOOST_DI_ARG_I(n,__VA_ARGS__)
 #define BOOST_DI_ARG_I(n, ...) BOOST_DI_CAT(BOOST_DI_CAT(BOOST_DI_ARG, n)(__VA_ARGS__,),)
 #define BOOST_DI_ARG0(p1, ...) p1
 #define BOOST_DI_ARG1(p1, p2, ...) p2
@@ -1101,7 +1096,6 @@ public:
 #define BOOST_DI_ARG7(p1, p2, p3, p4, p5, p6, p7, p8, ...) p8
 #define BOOST_DI_ARG8(p1, p2, p3, p4, p5, p6, p7, p8, p9, ...) p9
 #define BOOST_DI_ARG9(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, ...) p10
-
 #define BOOST_DI_REPEAT_N(i, m, ...) BOOST_DI_REPEAT_##i(m, __VA_ARGS__)
 #define BOOST_DI_REPEAT_1(m, ...) m(0, __VA_ARGS__)
 #define BOOST_DI_REPEAT_2(m, ...) m(0, __VA_ARGS__) m(1, __VA_ARGS__)
@@ -1127,7 +1121,6 @@ public:
     #define BOOST_DI_IBP_IS_VARIADIC_R_1 1,
     #define BOOST_DI_IBP_IS_VARIADIC_R_BOOST_DI_IBP_IS_VARIADIC_C 0,
     #define BOOST_DI_IBP(...) BOOST_DI_IBP_SPLIT(0, BOOST_DI_IBP_CAT(BOOST_DI_IBP_IS_VARIADIC_R_, BOOST_DI_IBP_IS_VARIADIC_C __VA_ARGS__))
-
     #define BOOST_DI_IIF(bit, t, f) BOOST_DI_IIF_OO((bit, t, f))
     #define BOOST_DI_IIF_OO(par) BOOST_DI_IIF_I ## par
     #define BOOST_DI_IIF_I(bit, t, f) BOOST_DI_IIF_ ## bit(t, f)
@@ -1138,11 +1131,7 @@ public:
     #define BOOST_DI_IBP_IS_VARIADIC_C(...) 1
     #define BOOST_DI_IBP_IS_VARIADIC_R_1 1,
     #define BOOST_DI_IBP_IS_VARIADIC_R_BOOST_DI_IBP_IS_VARIADIC_C 0,
-    #define BOOST_DI_IBP(...) \
-            BOOST_DI_IBP_SPLIT(0, BOOST_DI_CAT( \
-                BOOST_DI_IBP_IS_VARIADIC_R_, BOOST_DI_IBP_IS_VARIADIC_C __VA_ARGS__) \
-            )
-
+    #define BOOST_DI_IBP(...) BOOST_DI_IBP_SPLIT(0, BOOST_DI_CAT( BOOST_DI_IBP_IS_VARIADIC_R_, BOOST_DI_IBP_IS_VARIADIC_C __VA_ARGS__))
     #define BOOST_DI_IIF(bit, t, f) BOOST_DI_IIF_I(bit, t, f)
     #define BOOST_DI_IIF_I(bit, t, f) BOOST_DI_IIF_II(BOOST_DI_IIF_ ## bit(t, f))
     #define BOOST_DI_IIF_II(id) id
