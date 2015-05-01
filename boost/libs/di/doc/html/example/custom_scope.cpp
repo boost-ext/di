bad411de71b3535055f4792bf51e03cc8a4de3a1
Travis-CI <travis@ci.net>
2015-05-01 10:59:35 +0000

    doc regeneration

--- boost/libs/di/doc/html/example/custom_scope.cpp
+++ boost/libs/di/doc/html/example/custom_scope.cpp
@@ -47,8 +47,8 @@ public:
         };
 
     public:
-        /*<<define result_type>>*/
-        using result_type = custom_wrapper;
+        template<class>
+        using is_referable = std::false_type;
 
         void call(const entry&) {
             in_scope_ = true;
