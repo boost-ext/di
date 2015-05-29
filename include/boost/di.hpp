941fd207e36583a809755fda048d60b1cd29b954
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-05-29 17:50:05 +0100

    pph

--- include/boost/di.hpp
+++ include/boost/di.hpp
@@ -2766,6 +2766,22 @@ namespace boost { namespace di { inline namespace v1 { namespace core {
 
 BOOST_DI_HAS_METHOD(call_operator, operator());
 
+template<
+    class T
+  , class TName
+  , class TIsRoot
+  , class TDeps
+  , class TIgnore = std::false_type
+> struct arg_wrapper {
+    using type BOOST_DI_UNUSED = T;
+    using name BOOST_DI_UNUSED = TName;
+    using is_root BOOST_DI_UNUSED = TIsRoot;
+    using ignore = TIgnore;
+
+    template<class T_, class TName_, class TDefault_>
+    using resolve = decltype(core::binder::resolve<T_, TName_, TDefault_>((TDeps*)0));
+};
+
 template<class T>
 struct allow_void : T { };
 
@@ -3070,23 +3086,6 @@ struct from_injector { };
 struct from_deps { };
 struct init { };
 struct with_error { };
-
-template<
-    class T
-  , class TName
-  , class TIsRoot
-  , class TDeps
-  , class TIgnore = std::false_type
-> struct arg_wrapper {
-    using type BOOST_DI_UNUSED = T;
-    using name BOOST_DI_UNUSED = TName;
-    using is_root BOOST_DI_UNUSED = TIsRoot;
-    using ignore = TIgnore;
-
-    template<class T_, class TName_, class TDefault_>
-    using resolve =
-        decltype(core::binder::resolve<T_, TName_, TDefault_>((TDeps*)0));
-};
 
 template<class T, class TInjector>
 inline auto build(const TInjector& injector) noexcept {
