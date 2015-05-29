1a706d13578811cda81f431b3e4d74594261f9a8
Travis-CI <travis@ci.net>
2015-05-29 19:00:14 +0000

    doc regeneration

--- boost/libs/di/doc/html/example/extensions/contextual_bindings.cpp
+++ boost/libs/di/doc/html/example/extensions/contextual_bindings.cpp
@@ -10,65 +10,28 @@
 #include <cassert>
 #include <typeinfo>
 #include <map>
+#include <vector>
+#include <memory>
 //->
+
 #define BOOST_DI_CFG_NO_PREPROCESSED_HEADERS
-#include <boost/di.hpp>
+#include <boost/units/detail/utility.hpp>
+
+#include <boost/di/fwd.hpp>
+#include <boost/di/aux_/utility.hpp>
 
 namespace di = boost::di;
-
-struct c {
-    c(int i) {
-        assert(i == 3);
-    }
-};
-
-struct b {
-    b(c, int i) {
-        assert(i == 2);
-    }
-};
-
-struct a {
-    a(b, int i) {
-        assert(i == 1);
-    }
-};
-
-template<class>
-struct get_type;
-
-template<class T>
-struct get_type<di::aux::type<T>> {
-    using type = T;
-};
-
-class build_types : public di::config {
-public:
-    auto policies() noexcept {
-        return di::make_policies([&](auto type, auto dependency, auto... ctor) {
-            using arg = typename decltype(type)::type;
-            types[v[i - 1]].push_back(&typeid(arg));
-            auto ctor_size = sizeof...(ctor);
-            while (ctor_size--) {
-                v.insert((v.begin()+i), v[i-1]+1);
-            }
-            ++i;
-        });
-    }
-
-    std::map<int, std::vector<const std::type_info*>> types;
-
-private:
-    std::vector<int> v = { 0 };
-    int i = 1;
-};
 
 template<class>
 struct type_;
 
 template<class... Ts>
 struct type_<di::aux::type_list<Ts...>> {
-    using element_type = di::aux::type_list<Ts...>;
+    auto dump() const {
+        std::vector<const std::type_info*> v;
+        [](...){}((v.push_back(&typeid(Ts)), 0)...);
+        return v;
+    }
 
     template<class T>
     auto type() {
@@ -78,7 +41,9 @@ struct type_<di::aux::type_list<Ts...>> {
 
 template<class T>
 struct type {
-    using element_type = di::aux::type_list<T>;
+    auto dump() const {
+        return std::vector<const std::type_info*>{&typeid(T)};
+    }
 
     auto operator->() const {
         return std::make_unique<type_<di::aux::type_list<T>>>();
@@ -103,70 +68,95 @@ struct context_ {
 
 context_ context;
 
-template<class T, class R>
-struct all {
-    using type = R;
-    T object;
-};
-
-template<class R>
-struct then_ {
-    template<class T>
-    auto then(T value) {
-        return all<T, R>{value};
-    }
-};
-
-template<class T>
-then_<typename T::element_type> when(T&&) {
-    return {};
-}
-
-template<class TResult>
+template<class TExpected>
 struct contextual {
-    template<class... Ts>
-    explicit contextual(Ts&&... args)
-        : types{std::make_pair(create_types(typename Ts::type{}), args.object)...}
-    { }
-
-    template<class... Ts>
-    std::vector<const std::type_info*> create_types(const di::aux::type_list<Ts...>&) const {
-        return {&typeid(Ts)...};
-    }
-
     template<class TInjector>
-    auto operator()(const TInjector& injector) const -> TResult {
-        const auto& call_stack = injector.types;
-
-        for (const auto& el : types) {
-            auto level = 0;
-            auto found = true;
-            for (const auto& t : el.first) {
-                const auto& cs = call_stack.at(level);
-                if (std::find(cs.begin(), cs.end(), t) == cs.end()) {
-                    found = false;
-                    break;
-                }
-                ++level;
-            }
-
-            if (found) {
-                return el.second;
-            }
-        }
-
+    auto operator()(const TInjector&) const -> TExpected {
         return {};
     }
 
-    std::vector<std::pair<std::vector<const std::type_info*>, TResult>> types;
+    std::vector<std::pair<std::vector<const std::type_info*>, TExpected>> values_;
+};
+
+struct contextual_ext {
+    template<class>
+    struct extension;
+
+    template<class TScope, class TExpected, class... Ts>
+    struct extension<boost::di::core::dependency<TScope, TExpected, Ts...>> {
+        template<class TContext>
+        auto when(const TContext& context) noexcept {
+            values_.push_back(std::make_pair(context.dump(), TExpected{}));
+            auto& self = static_cast<boost::di::core::dependency<TScope, TExpected, Ts...>&>(*this);
+            return self;
+        }
+
+        template<class T>
+        auto then(T&& value) noexcept {
+            values_.back().second = std::forward<T>(value);
+            auto& self = static_cast<boost::di::core::dependency<TScope, TExpected, Ts...>&>(*this);
+            return self;//.to(contextual<TExpected>{values_});
+        }
+
+        template<class T, class TProvider>
+        auto create(const TProvider& provider) const noexcept {
+            return 42;
+        }
+
+        std::vector<std::pair<std::vector<const std::type_info*>, TExpected>> values_;
+    };
+};
+
+/*<<define dependency extensions>>*/
+#define BOOST_DI_CFG_DEPENDENCY_EXTENSIONS ::contextual_ext
+
+#include <boost/di.hpp>
+
+class build_types : public di::config {
+public:
+/*    auto policies() noexcept {*/
+        //return di::make_policies([&](auto type, auto de, auto... ctor) {
+            //using arg = typename decltype(type)::type;
+            //types[v[i - 1]].push_back(&typeid(arg));
+            //auto ctor_size = sizeof...(ctor);
+            //while (ctor_size--) {
+                //v.insert((v.begin()+i), v[i-1]+1);
+            //}
+            //++i;
+        //});
+    /*}*/
+
+    std::map<int, std::vector<const std::type_info*>> types;
+
+private:
+    //std::vector<int> v = { 0 };
+    //int i = 1;
+};
+
+struct c {
+    c(int i) {
+        assert(i == 3);
+    }
+};
+
+struct b {
+    b(c, int i) {
+        assert(i == 2);
+    }
+};
+
+struct a {
+    a(b, int i) {
+        assert(i == 1);
+    }
 };
 
 int main() {
-    auto injector = di::make_injector<build_types>(
+    //auto injector = di::make_injector<build_types>(
+    auto injector = di::make_injector(
         di::bind<int>.when(context = type<a>() -> type<b>() -> type<c>()).then(3)
                      .when(context = type<a>() -> type<b>()).then(2)
                      .when(context = type<a>()).then(1)
-        )
     );
 
     injector.create<a>();
