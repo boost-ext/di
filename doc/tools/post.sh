13dce4fd16d35d97b227ceac63d3c82e1440deae
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-06-08 12:29:44 +0100

    doc -> post processing

---
+++ doc/tools/post.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HTML="`readlink -f \`dirname $0\`/../html`"
+
+#index.html
+sed -i -e 's/<div class=\"toc\">/<div class=\"toc\">\n<__badges__>\n<__search__>/' $HTML/index.html
+sed -i -e '/<__badges__>/{r tools/data/badges.html' -e 'd}' $HTML/index.html
+sed -i -e '/<__search__>/{r tools/data/search.html' -e 'd}' $HTML/index.html
+
+#*.html
+find $HTML/di -iname "*.html" | xargs sed -i -e 's/<\/div><\/div><\/div>/<__search__><\/div><\/div><\/div>/'
+find $HTML/di -iname "*.html" | xargs sed -i -e '/<__search__>/{r tools/data/search.html' -e 'd}'
+find $HTML -iname "*.html" | xargs sed -i -e 's/<\/body>/<__comments__>\n<\/body>/'
+find $HTML -iname "*.html" | xargs sed -i -e '/<__comments__>/{r tools/data/comments.html' -e 'd}'
+
