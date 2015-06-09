ba0617bbb4aeb9c9c0d9a167ee3c449ea03c6c05
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-06-09 13:52:50 +0100

    [doc] fix for missing boost links

--- doc/tools/post.sh
+++ doc/tools/post.sh
@@ -12,7 +12,7 @@ find $HTML/di -iname "*.html" | xargs sed -i -e 's/<\/div><\/div><\/div>/<__sear
 find $HTML/di -iname "*.html" | xargs sed -i -e '/<__search__>/{r tools/data/search.html' -e 'd}'
 find $HTML -iname "*.html" | xargs sed -i -e 's/<\/body>/<__comments__>\n<\/body>/'
 find $HTML -iname "*.html" | xargs sed -i -e '/<__comments__>/{r tools/data/comments.html' -e 'd}'
-find $HTML/di -iname "*.html" | xargs sed -i -e 's/\.\.\/\.\.\/\.\.\/\.\.\/\.\.\/index\.html/http:\/\/boost.org/'
-find $HTML/di -iname "*.html" | xargs sed -i -e 's/\.\.\/\.\.\/\.\.\/\.\.\/\.\.\/libs\/libraries\.htm/http:\/\/boost.org\/doc\/libs/'
-find $HTML/di -iname "*.html" | xargs sed -i -e 's/\.\.\/\.\.\/\.\.\/\.\.\/\.\.\/more\/index\.html/http:\/\/boost.org\/users\/index.html/'
+find $HTML/di -iname "*.html" | xargs sed -i -e 's/\.\.\/\.\.\/\.\.\/\.\.\/index\.html/http:\/\/boost.org/'
+find $HTML/di -iname "*.html" | xargs sed -i -e 's/\.\.\/\.\.\/\.\.\/\.\.\/libs\/libraries\.htm/http:\/\/boost.org\/doc\/libs/'
+find $HTML/di -iname "*.html" | xargs sed -i -e 's/\.\.\/\.\.\/\.\.\/\.\.\/more\/index\.htm/http:\/\/boost.org\/users\/index.html/'
 
