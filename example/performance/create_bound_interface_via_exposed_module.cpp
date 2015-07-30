3417572157153d8dcf3c7f60f5e7205244eb2867
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-07-30 15:05:18 +0100

    [example] performance examples for wandobox

---
+++ example/performance/create_bound_interface_via_exposed_module.cpp
@@ -0,0 +1,34 @@
+//
+// Copyright (c) 2012-2015 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <cstdlib>
+#include <string>
+#include <memory>
+#include <boost/di.hpp>
+
+namespace di = boost::di;
+
+struct interface { virtual ~interface() noexcept = default; virtual void dummy() = 0;};
+struct implementation : interface { void dummy() override { } };
+
+struct module {
+    di::injector<interface> configure() const {
+        return di::make_injector(
+            di::bind<interface, implementation>
+        );
+    }
+};
+
+auto test() {
+    auto injector = di::make_injector(module{});
+    return injector.create<std::unique_ptr<interface>>();
+}
+
+
+int main(int, char** argv) {
+    std::system(("gdb -batch -ex 'file " + std::string{argv[0]} + "' -ex 'disassemble test'").c_str());
+}
+
---
+++ example/performance/create_named_type.cpp
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 2012-2015 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <cstdlib>
+#include <string>
+#include <boost/di.hpp>
+
+namespace di = boost::di;
+
+auto my_int = []{};
+
+struct c {
+    BOOST_DI_INJECT(c
+        , (named = my_int) int i)
+        : i(i)
+    { }
+
+    int i = 0;
+};
+
+auto test() {
+    auto injector = di::make_injector(
+        di::bind<int>().named(my_int).to(42)
+    );
+
+    return injector.create<c>().i;
+}
+
+int main(int, char** argv) {
+    std::system(("gdb -batch -ex 'file " + std::string{argv[0]} + "' -ex 'disassemble test'").c_str());
+}
+
