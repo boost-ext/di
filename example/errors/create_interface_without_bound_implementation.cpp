480d42fd124c9741ebe63c89ed5214592b997de3
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-07-30 16:29:47 +0100

    [doc] errors for wandbox

--- example/errors/create_interface_without_bound_implementation.cpp
+++ example/errors/create_interface_without_bound_implementation.cpp
@@ -4,18 +4,18 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#include "boost/di.hpp"
+#include <memory>
+#include <boost/di.hpp>
 
 namespace di = boost::di;
 
-struct i { virtual ~i() noexcept = default; virtual void dummy() = 0; };
-struct impl : i { void dummy() override { } };
+struct interface { virtual ~interface() noexcept = default; virtual void dummy() = 0; };
 
-struct c {
-    c(i*) { }
+struct example {
+    example(std::unique_ptr<interface>) { }
 };
 
-test create_polymorphic_type_without_binding = [] {
-    di::make_injector().create<c>();
-};
+int main() {
+    di::make_injector().create<example>();
+}
 
---
+++ example/errors/create_not_bound_object_with_all_bound_policy.cpp
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2012-2015 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/di.hpp>
+
+namespace di = boost::di;
+
+class must_be_bound : public di::config {
+public:
+    static auto policies(...) {
+        using namespace di::policies;
+        return di::make_policies(
+            constructible(is_bound<di::_>{})
+        );
+    }
+};
+
+struct example {
+    example(int, double, float) { }
+};
+
+int main() {
+    auto injector = di::make_injector<must_be_bound>();
+    injector.create<example>();
+}
+
