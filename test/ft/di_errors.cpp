9dfb8fb5bd32343eab1df74daa94595a4f6d9066
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-08-02 21:49:01 +0100

    [ft] missing memory include

--- test/ft/di_errors.cpp
+++ test/ft/di_errors.cpp
@@ -5,6 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include "boost/di/aux_/compiler.hpp"
+#include <memory>
 #include <cstdlib>
 #include <cstdio>
 #include <fstream>
