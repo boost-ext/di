442bdc4a03ae60e0b1e24eb209c99ff9cdadcdae
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-08-11 11:44:52 +0100

    [ut] fix for ut

--- test/ut/providers/heap.cpp
+++ test/ut/providers/heap.cpp
@@ -6,38 +6,39 @@
 //
 #include <memory>
 #include "boost/di/providers/heap.hpp"
+#include "boost/di/aux_/type_traits.hpp"
 #include "boost/di/type_traits/memory_traits.hpp"
 
 namespace boost { namespace di { inline namespace v1 { namespace providers {
 
-auto test_heap = [](auto type, auto init, auto memory, auto... args) {
-    using T = typename decltype(type)::type;
+template<class T, class TInit, class TMemory, class... TArgs>
+auto test_heap(const aux::type<T>&, const TInit& init, const TMemory& memory, const TArgs&... args) {
     std::unique_ptr<T> object{heap{}.get<T, T>(init, memory, args...)};
     expect(object.get());
-};
+}
 
 test get_no_args = [] {
     struct c { };
-    test_heap(test_type<int>{}, type_traits::direct{}, type_traits::stack{});
-    test_heap(test_type<int>{}, type_traits::direct{}, type_traits::heap{});
-    test_heap(test_type<int>{}, type_traits::uniform{}, type_traits::stack{});
-    test_heap(test_type<int>{}, type_traits::uniform{}, type_traits::heap{});
-    test_heap(test_type<c>{}, type_traits::direct{}, type_traits::stack{});
-    test_heap(test_type<c>{}, type_traits::direct{}, type_traits::heap{});
-    test_heap(test_type<c>{}, type_traits::uniform{}, type_traits::stack{});
-    test_heap(test_type<c>{}, type_traits::uniform{}, type_traits::heap{});
+    test_heap(aux::type<int>{}, type_traits::direct{}, type_traits::stack{});
+    test_heap(aux::type<int>{}, type_traits::direct{}, type_traits::heap{});
+    test_heap(aux::type<int>{}, type_traits::uniform{}, type_traits::stack{});
+    test_heap(aux::type<int>{}, type_traits::uniform{}, type_traits::heap{});
+    test_heap(aux::type<c>{}, type_traits::direct{}, type_traits::stack{});
+    test_heap(aux::type<c>{}, type_traits::direct{}, type_traits::heap{});
+    test_heap(aux::type<c>{}, type_traits::uniform{}, type_traits::stack{});
+    test_heap(aux::type<c>{}, type_traits::uniform{}, type_traits::heap{});
 };
 
 test get_with_args = [] {
     struct direct { direct(int, double) { } };
     struct uniform { int i = 0; double d = 0.0; };
-    test_heap(test_type<direct>{}, type_traits::direct{}, type_traits::stack{}, int{}, double{});
-    test_heap(test_type<direct>{}, type_traits::direct{}, type_traits::heap{}, int{}, double{});
-    test_heap(test_type<direct>{}, type_traits::uniform{}, type_traits::stack{}, int{}, double{});
-    test_heap(test_type<direct>{}, type_traits::uniform{}, type_traits::heap{}, int{}, double{});
+    test_heap(aux::type<direct>{}, type_traits::direct{}, type_traits::stack{}, int{}, double{});
+    test_heap(aux::type<direct>{}, type_traits::direct{}, type_traits::heap{}, int{}, double{});
+    test_heap(aux::type<direct>{}, type_traits::uniform{}, type_traits::stack{}, int{}, double{});
+    test_heap(aux::type<direct>{}, type_traits::uniform{}, type_traits::heap{}, int{}, double{});
 #if !defined(_MSC_VER)
-    test_heap(test_type<uniform>{}, type_traits::uniform{}, type_traits::stack{}, int{}, double{});
-    test_heap(test_type<uniform>{}, type_traits::uniform{}, type_traits::heap{}, int{}, double{});
+    test_heap(aux::type<uniform>{}, type_traits::uniform{}, type_traits::stack{}, int{}, double{});
+    test_heap(aux::type<uniform>{}, type_traits::uniform{}, type_traits::heap{}, int{}, double{});
 #endif
 };
 
--- test/ut/providers/stack_over_heap.cpp
+++ test/ut/providers/stack_over_heap.cpp
@@ -6,39 +6,40 @@
 //
 #include <memory>
 #include "boost/di/providers/stack_over_heap.hpp"
+#include "boost/di/aux_/type_traits.hpp"
 
 namespace boost { namespace di { inline namespace v1 { namespace providers {
 
-auto test_heap = [](auto type, auto init, auto... args) {
-    using T = typename decltype(type)::type;
+template<class T, class TInit, class... TArgs>
+auto test_heap(const aux::type<T>&, const TInit& init, const TArgs&... args) {
     std::unique_ptr<T> object{stack_over_heap{}.get<T, T>(init, type_traits::heap{}, args...)};
     expect(object.get());
-};
+}
 
-auto test_stack = [](auto type, auto init, auto... args) {
-    using T = typename decltype(type)::type;
+template<class T, class TInit, class... TArgs>
+auto test_stack(const aux::type<T>&, const TInit& init, const TArgs&... args) {
     T object = stack_over_heap{}.get<T, T>(init, type_traits::stack{}, args...);
     (void)object;
-};
+}
 
 test get_no_args = [] {
     struct c { };
-    test_stack(test_type<int>{}, type_traits::direct{});
-    test_heap(test_type<int>{}, type_traits::uniform{});
-    test_stack(test_type<int>{}, type_traits::uniform{});
-    test_heap(test_type<c>{}, type_traits::direct{});
-    test_stack(test_type<c>{}, type_traits::direct{});
-    test_heap(test_type<c>{}, type_traits::uniform{});
-    test_stack(test_type<c>{}, type_traits::uniform{});
+    test_stack(aux::type<int>{}, type_traits::direct{});
+    test_heap(aux::type<int>{}, type_traits::uniform{});
+    test_stack(aux::type<int>{}, type_traits::uniform{});
+    test_heap(aux::type<c>{}, type_traits::direct{});
+    test_stack(aux::type<c>{}, type_traits::direct{});
+    test_heap(aux::type<c>{}, type_traits::uniform{});
+    test_stack(aux::type<c>{}, type_traits::uniform{});
 };
 
 test get_with_args = [] {
     struct direct { direct(int, double) { } };
     struct uniform { int i = 0; double d = 0.0; };
-    test_stack(test_type<direct>{}, type_traits::direct{}, int{}, double{});
-    test_heap(test_type<direct>{}, type_traits::uniform{}, int{}, double{});
+    test_stack(aux::type<direct>{}, type_traits::direct{}, int{}, double{});
+    test_heap(aux::type<direct>{}, type_traits::uniform{}, int{}, double{});
 #if !defined(_MSC_VER)
-    test_heap(test_type<uniform>{}, type_traits::uniform{}, int{}, double{});
+    test_heap(aux::type<uniform>{}, type_traits::uniform{}, int{}, double{});
 #endif
 };
 
