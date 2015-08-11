a168e7c37a88072189d046da6c83600b943cbac0
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-08-11 11:49:30 +0100

    [test] simplify test framework even more -> just assert for now

--- test/common/test.hpp
+++ test/common/test.hpp
@@ -7,34 +7,13 @@
 #ifndef BOOST_DI_TEST_HPP
 #define BOOST_DI_TEST_HPP
 
-#include <iostream>
+#undef NDEBUG
+#include <cassert>
 
-#define expect(...) \
-    do { \
-        if (!(__VA_ARGS__)) { \
-            std::cerr << __FILE__ << " " << __LINE__ << " expect \"" << #__VA_ARGS__ << "\"" << std::endl; \
-            std::abort(); \
-        } \
-    } while (false)
-
-#define expect_eq(t1, t2) \
-    do { \
-        if (t1 != t2) { \
-            std::cerr << __FILE__ << " " << __LINE__ << " expect_eq \"" << #t1 << "\" \"" << #t2 << "\"" << std::endl; \
-            std::abort(); \
-        } \
-    } while (false)
-
-#define expect_neq(t1, t2) \
-    do { \
-        if (t1 == t2) { \
-            std::cerr << __FILE__ << " " << __LINE__ << " expect_neq \"" << #t1 << "\" \"" << #t2 << "\"" << std::endl; \
-            std::abort(); \
-        } \
-    } while (false)
-
-#define static_expect(...) \
-    static_assert((__VA_ARGS__), "static_expect")
+#define expect(...) assert((__VA_ARGS__))
+#define expect_eq(t1, t2) assert((t1 == t2))
+#define expect_neq(t1, t2) assert((t1 != t2))
+#define static_expect(...) static_assert((__VA_ARGS__), "")
 
 struct test {
     template<class Test>
--- test/ft/di_config_global_provider.cpp
+++ test/ft/di_config_global_provider.cpp
@@ -5,6 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #define BOOST_DI_CFG global_provider
+#include <utility>
 #include <boost/di.hpp>
 
 namespace di = boost::di;
