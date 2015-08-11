ddac2e46b5414c3bcaa46e7e5cba7d8659fe7d3a
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-08-11 13:42:05 +0100

    [ut] string includes fix

--- test/ut/scopes/external.cpp
+++ test/ut/scopes/external.cpp
@@ -5,6 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include <memory>
+#include <string>
 #include <functional>
 #include "boost/di/scopes/external.hpp"
 #include "common/fakes/fake_provider.hpp"
--- test/ut/scopes/shared.cpp
+++ test/ut/scopes/shared.cpp
@@ -4,7 +4,6 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#include <string>
 #include "boost/di/scopes/shared.hpp"
 #include "common/fakes/fake_provider.hpp"
 
