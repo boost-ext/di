9c0b381825c96b6e417b354bebe89b64f9c3d1a3
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-08-11 11:56:26 +0100

    [ut] fix for missing string include for di_bind

--- test/ft/di_bind.cpp
+++ test/ft/di_bind.cpp
@@ -5,6 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include <memory>
+#include <string>
 #include "boost/di.hpp"
 
 namespace di = boost::di;
