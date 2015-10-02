40075943aad8097f81837e6805083b11068587d3
Travis-CI <travis@ci.net>
2015-10-02 15:48:28 +0000

    update

--- cpp14/boost/libs/di/doc/html/example/multiple_bindings.cpp
+++ cpp14/boost/libs/di/doc/html/example/multiple_bindings.cpp
@@ -46,7 +46,7 @@ struct example {
 
 int main() {
     auto injector = di::make_injector(
-        di::bind<int*[]>().to({1, 2, 3, 5, 8, 13, 21})
+        di::bind<int[]>().to({1, 2, 3, 5, 8, 13, 21})
       , di::bind<interface*[]>().to<implementation1, implementation2, interface, di::named<class Implementation2>>()
       , di::bind<interface>().to<implementation1>() // -------------------^                      ^
       , di::bind<interface>().named<class Implementation2>().to<implementation2>() // -----------/
