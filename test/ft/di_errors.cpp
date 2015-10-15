94f7e7ec7ef64daf9071d86c2dff8d92d25c80b7
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-10-15 21:46:51 +0100

    style

--- test/ft/di_errors.cpp
+++ test/ft/di_errors.cpp
@@ -1,4 +1,5 @@
-// // Copyright (c) 2012-2015 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Copyright (c) 2012-2015 Krzysztof Jusiak (krzysztof at jusiak dot net)
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
