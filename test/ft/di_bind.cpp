10b65ade7e725cf91e6cae88faa87d78acf31d58
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-11-11 08:55:00 +0000

    [test] bind_non_owning_ptr

--- test/ft/di_bind.cpp
+++ test/ft/di_bind.cpp
@@ -235,6 +235,14 @@ test bind_non_interface_in_singleton_scope = [] {
   expect(injector.create<std::shared_ptr<c>>() == injector.create<std::shared_ptr<c>>());
 };
 
+// test bind_interface_implementation_in_singleton_scope = [] {
+// struct c {
+// c(std::shared_ptr<i1> sp1, std::shared_ptr<impl1> sp2) { expect(sp1 == sp2); }
+//};
+// auto injector = di::make_injector(di::bind<i1>().to<impl1>().in(di::singleton));
+// injector.create<c>();
+//};
+
 test bind_const_ref_type_in_singleton_scope = [] {
   struct c_inject {
     BOOST_DI_INJECT(c_inject, const int& i1, const int& i2) { expect(&i1 == &i2); }
@@ -478,6 +486,21 @@ test bind_function_to_callable = [] {
       di::make_injector(di::bind<std::function<int(int)>>().to([](const auto&) { return [](int i) { return i; }; }));
 
   injector.create<functions>();
+};
+
+test bind_non_owning_ptr = [] {
+  using Pointer = int;
+  constexpr auto i = 42;
+  struct c {
+    c(Pointer& ptr) { expect(i == ptr); }
+  };
+  auto module = [](Pointer* ptr) { return di::bind<Pointer>.to(*ptr); };
+  di::aux::owner<Pointer*> ptr{new Pointer{i}};
+  auto injector = di::make_injector(module(ptr));
+
+  injector.create<c>();
+
+  delete ptr;
 };
 
 test multi_bindings_empty = [] {
