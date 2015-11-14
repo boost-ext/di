6a0214cba497a892122a2f0fa999a083e7b66fb5
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-11-14 10:18:58 +0000

    [example] missing include

--- example/quick_user_guide/scopes4.cpp
+++ example/quick_user_guide/scopes4.cpp
@@ -5,6 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include <memory>
+#include <type_traits>
 #include <cassert>
 #include <boost/di.hpp>
 
