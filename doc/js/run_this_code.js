84f7f8db4a33e6ea5cee051325d68603d43e8d5a
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-11-25 12:02:39 +0000

    [doc] support for compiler/log_level/version when run_this_code

--- doc/js/run_this_code.js
+++ doc/js/run_this_code.js
@@ -24,6 +24,9 @@ function toggle(id, file) {
     cpp_output[id].toTextArea();
     document.getElementById("output_" + id).style.display = 'none';
     document.getElementById("compile_and_run_" + id).remove();
+    document.getElementById("compiler_" + id).remove();
+    document.getElementById("log_level_" + id).remove();
+    document.getElementById("version_" + id).remove();
     document.getElementById("powered_by_" + id).remove();
 }
 
@@ -31,19 +34,124 @@ function show(id, file) {
     document.getElementById('run_it_btn_' + id).firstChild.data = 'Exit';
     document.getElementById("run_it_btn_" + id).onclick = function() { toggle(id, file); };
     document.getElementById("code_listing_" + id).style.display = 'none';
+
     var powered_by = document.createElement("text");
     var powered_text = document.createTextNode("Powered by Wandbox.");
     powered_by.appendChild(powered_text);
     powered_by.setAttribute("id", "powered_by_" + id);
     powered_by.setAttribute('style', 'font-size: 10px; text-align: right; display: block;');
+
     var compile_btn = document.createElement("BUTTON");
     var compile_txt = document.createTextNode("Compile & Run (Ctrl+Enter)");
     compile_btn.setAttribute("id", "compile_and_run_" + id);
     compile_btn.setAttribute("class", "TryItBtn");
     compile_btn.appendChild(compile_txt);
     compile_btn.onclick = function() { compile_and_run(id) };
+
+    var compiler_btn = document.createElement("SELECT");
+    compiler_btn.setAttribute("id", "compiler_" + id);
+    compiler_btn.setAttribute("class", "Option");
+	{
+	var option = document.createElement("OPTION");
+	option.value = "clang-head";
+	option.text = "clang-head";
+	compiler_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.value = "clang-3.7";
+	option.text = "clang-3.7";
+	compiler_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.value = "clang-3.6";
+	option.text = "clang-3.6";
+	compiler_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.value = "clang-3.5";
+	option.text = "clang-3.5";
+	compiler_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.value = "clang-3.4";
+	option.text = "clang-3.4";
+	compiler_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.value = "gcc-head";
+	option.text = "gcc-head";
+	compiler_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.value = "gcc-5.2";
+	option.text = "gcc-5.2";
+	compiler_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.value = "gcc-5.1";
+	option.text = "gcc-5.1";
+	compiler_btn.appendChild(option);
+	}
+
+    compiler_btn.onclick = function() { };
+
+    var log_level_btn = document.createElement("SELECT");
+
+    log_level_btn.setAttribute("id", "log_level_" + id);
+    log_level_btn.setAttribute("class", "Option");
+	{
+	var option = document.createElement("OPTION");
+	option.text = "BOOST_DI_CFG_DIAGNOSTICS_LEVEL=0";
+	option.value = "0";
+	log_level_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.text = "BOOST_DI_CFG_DIAGNOSTICS_LEVEL=1 (default)";
+	option.value = "1";
+	log_level_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.text = "BOOST_DI_CFG_DIAGNOSTICS_LEVEL=2";
+	option.value = "2";
+	log_level_btn.appendChild(option);
+	}
+
+	log_level_btn.selectedIndex = 1;
+
+    log_level_btn.onclick = function() { };
+
+    var version_btn = document.createElement("SELECT");
+    version_btn.setAttribute("id", "version_" + id);
+    version_btn.setAttribute("class", "Option");
+	{
+	var option = document.createElement("OPTION");
+	option.text = "head";
+	option.value = "cpp14";
+    version_btn.appendChild(option);
+	}
+	{
+	var option = document.createElement("OPTION");
+	option.text = "v0.5.0";
+	option.value = "v0.5.0";
+    version_btn.appendChild(option);
+	}
+    version_btn.onclick = function() { };
+
     document.getElementById("run_it_btn_" + id).parentNode.insertBefore(compile_btn, document.getElementById("run_it_btn_" + id).nextSibling);
-    document.getElementById("run_it_btn_" + id).parentNode.insertBefore(powered_by, compile_btn.nextSibling);
+    document.getElementById("run_it_btn_" + id).parentNode.insertBefore(compiler_btn, compile_btn.nextSibling);
+    document.getElementById("run_it_btn_" + id).parentNode.insertBefore(log_level_btn, compiler_btn.nextSibling);
+    document.getElementById("run_it_btn_" + id).parentNode.insertBefore(version_btn, log_level_btn.nextSibling);
+    document.getElementById("run_it_btn_" + id).parentNode.insertBefore(powered_by, version_btn.nextSibling);
+
     get_example(id, file);
     compile_and_run(id);
 }
@@ -77,11 +185,11 @@ function compile_and_run(id) {
           "code" : cpp_code[id].getValue()
         , "codes" : [{
               "file" : "boost/di.hpp"
-            , "code" : get_cpp_file("https://raw.githubusercontent.com/krzysztof-jusiak/di/cpp14/include/boost/di.hpp")
+            , "code" : get_cpp_file("https://raw.githubusercontent.com/krzysztof-jusiak/di/" + document.getElementById("version_" + id).value + "/include/boost/di.hpp")
            }]
-         , "options": "warning,cpp-pedantic-errors,optimize,boost-nothing,c++14"
-         , "compiler": "clang-head"
-         , "compiler-option-raw": "-I."
+         , "options": "warning,cpp-pedantic-errors,optimize,boost-nothing,c++1y"
+		 , "compiler" : document.getElementById("compiler_" + id).value
+         , "compiler-option-raw": "-I." + "\n" + "-DBOOST_DI_CFG_DIAGNOSTICS_LEVEL=" + document.getElementById("log_level_" + id).value
     }));
 }
 
