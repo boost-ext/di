fcc6ced60cf53e755cf66e6d702dfea2c0199023
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-11-29 09:46:11 +0000

    [bindings] example with deduced bind<>.to(42)

--- example/quick_user_guide/bindings2.cpp
+++ example/quick_user_guide/bindings2.cpp
@@ -4,30 +4,13 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#include <memory>
 #include <cassert>
 #include <boost/di.hpp>
 
 namespace di = boost::di;
 
-struct i1 {
-  virtual ~i1() noexcept = default;
-  virtual void dummy1() = 0;
-};
-struct i2 {
-  virtual ~i2() noexcept = default;
-  virtual void dummy2() = 0;
-};
-struct impl : i1, i2 {
-  void dummy1() override {}
-  void dummy2() override {}
-};
-
 int main() {
-  auto injector = di::make_injector(di::bind<i1, i2>().to<impl>());
+  auto injector = di::make_injector(di::bind<int>().to(42));
 
-  auto object1 = injector.create<std::shared_ptr<i1>>();
-  auto object2 = injector.create<std::shared_ptr<i2>>();
-  assert(dynamic_cast<impl*>(object1.get()));
-  assert(dynamic_cast<impl*>(object2.get()));
+  assert(42 == injector.create<int>());
 }
--- example/quick_user_guide/bindings4.cpp
+++ example/quick_user_guide/bindings4.cpp
@@ -4,13 +4,17 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include <type_traits>
 #include <cassert>
 #include <boost/di.hpp>
 
 namespace di = boost::di;
 
+template <int N>
+using int_ = std::integral_constant<int, N>;
+
 int main() {
-  auto injector = di::make_injector(di::bind<int>().to(42));
+  auto injector = di::make_injector(di::bind<int>().to<int_<42>>());
 
   assert(42 == injector.create<int>());
 }
