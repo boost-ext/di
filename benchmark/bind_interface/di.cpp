63a2f2995a3000c9a860e9ea6d0bbdaf9a112d1c
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-02-18 14:18:19 +0000

    [benchmark] di fix

--- benchmark/bind_interface/di.cpp
+++ benchmark/bind_interface/di.cpp
@@ -1,8 +1,19 @@
+//
+// Copyright (c) 2012-2016 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//<-
 #include <memory>
+#include <cstdlib>
+#include <string>
+//->
 #include <boost/di.hpp>
 
 namespace di = boost::di;
 
+//<-
 struct I {
   virtual ~I() noexcept = default;
   virtual void dummy() = 0;
@@ -11,11 +22,35 @@ struct Impl : I {
   BOOST_DI_INJECT(Impl) {}
   void dummy() override {}
 };
+//->
 
-auto module = [] { return di::make_injector(di::bind<I>.to<Impl>()); };
+auto module = [] {
+  // clang-format off
+  return di::make_injector(
+    di::bind<I>.to<Impl>()
+  );
+  // clang-format on
+};
 
-int main() {
+auto test() {
   auto injector = di::make_injector(module());
-  std::unique_ptr<I> object{injector.create<I *>()};
-  object->dummy();
+  return injector.create<std::unique_ptr<I>>();
 }
+
+/**
+ * ASM x86-64 (same as `std::make_unique<implementation>()`)
+ *
+ * push   %rbx
+ * mov    %rdi,%rbx
+ * mov    $0x8,%edi
+ * callq  0x4009f0 <_Znwm@plt>
+ * movq   $0x400e78,(%rax)
+ * mov    %rax,(%rbx)
+ * mov    %rbx,%rax
+ * pop    %rbx
+ * ret
+ */
+
+//<-
+int main() { std::system(("gdb -batch -ex 'file " + std::string{argv[0]} + "' -ex 'disassemble test'").c_str()); }
+//->
