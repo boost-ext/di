6685c32d967051feabedbae1d0be94fb93bef5f0
Continuous Integration <krzysztof@jusiak.net>
2016-02-22 23:34:48 +0000

    doc update

--- site/boost/js/utility.js
+++ site/boost/js/utility.js
@@ -5,4 +5,10 @@ $(document).ready(function () {
       $(this).replaceWith(html);
     });
 });
-
+$(document).ready(function () {
+    $('img[alt="YOUTUBE"]').each(function () {
+        var id = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
+        var video = '<iframe style="width: 100%;height: 450px;" src="https://www.youtube.com/embed/' + id + '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
+        $(this).replaceWith(video);
+    });
+});
