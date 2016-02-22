0be28d97b0745c711a4343c27f8604ccddf1e198
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-02-22 21:26:07 +0000

    [benchmark] asm

--- benchmark/bind_value/dicpp.cpp
+++ benchmark/bind_value/dicpp.cpp
@@ -24,5 +24,50 @@ int main() {}
 /**
  * ASM x86-64
  *
+ * push   %rbx
+ * sub    $0xb0,%rsp
+ * lea    0x20(%rsp),%rbx
+ * xor    %esi,%esi
+ * mov    %rbx,%rdi
+ * callq  0x40af50 <_ZN2di8injectorC2ENS0_8localityE>
+ * movq   $0x404b10,(%rsp)
+ * movq   $0x40ab00,0x18(%rsp)
+ * movq   $0x40ab10,0x10(%rsp)
+ * mov    %rbx,%rdi
+ * callq  0x404b10 <_Z6moduleRN2di8registryE>
+ * mov    0x10(%rsp),%rax
+ * test   %rax,%rax
+ * je     0x404d61 <_Z4testv+81>
+ * lea    (%rsp),%rdi
+ * mov    $0x3,%edx
+ * mov    %rdi,%rsi
+ * callq  *%rax
+ * lea    0x20(%rsp),%rdi
+ * callq  0x40ab40 <_ZNK2di8registry9constructIiEENS_6traits13returned_typeIT_E4typeEv>
+ * mov    %eax,%ebx
+ * lea    0x20(%rsp),%rdi
+ * callq  0x40b390 <_ZN2di8registryD2Ev>
+ * mov    %ebx,%eax
+ * add    $0xb0,%rsp
+ * pop    %rbx
+ * retq
+ * mov    %rax,%rbx
+ * mov    0x10(%rsp),%rcx
+ * test   %rcx,%rcx
+ * je     0x404daa <_Z4testv+154>
+ * lea    (%rsp),%rdi
+ * mov    $0x3,%edx
+ * mov    %rdi,%rsi
+ * callq  *%rcx
+ * jmp    0x404daa <_Z4testv+154>
+ * mov    %rax,%rdi
+ * callq  0x405f60 <__clang_call_terminate>
+ * mov    %rax,%rbx
+ * lea    0x20(%rsp),%rdi
+ * callq  0x40b390 <_ZN2di8registryD2Ev>
+ * mov    %rbx,%rdi
+ * callq  0x404840 <_Unwind_Resume@plt>
+ * mov    %rax,%rdi
+ * callq  0x405f60 <__clang_call_terminate>
  */
 //->
--- benchmark/bind_value/fruit.cpp
+++ benchmark/bind_value/fruit.cpp
@@ -24,5 +24,69 @@ int main() {}
 /**
  * ASM x86-64
  *
+ * push   %rbp
+ * push   %r14
+ * push   %rbx
+ * sub    $0xd0,%rsp
+ * lea    0x8(%rsp),%rbx
+ * mov    %rbx,%rdi
+ * callq  0x400cd0 <_Z6modulev>
+ * lea    0xc8(%rsp),%rdi
+ * mov    %rbx,%rsi
+ * callq  0x401170 <_ZN5fruit8InjectorIJiEEC2ENS_9ComponentIJiEEE>
+ * mov    0xb0(%rsp),%rdi
+ * test   %rdi,%rdi
+ * je     0x400efa <_Z4testv+58>
+ * callq  0x400b10 <_ZdlPv@plt>
+ * mov    0x98(%rsp),%rdi
+ * test   %rdi,%rdi
+ * je     0x400f0c <_Z4testv+76>
+ * callq  0x400b10 <_ZdlPv@plt>
+ * mov    0x50(%rsp),%rdi
+ * test   %rdi,%rdi
+ * je     0x400f1b <_Z4testv+91>
+ * callq  0x400b10 <_ZdlPv@plt>
+ * mov    0xc8(%rsp),%rbx
+ * mov    %rbx,%rdi
+ * callq  0x4015e0
+ * <_ZN5fruit4impl15InjectorStorage3getIiEENS0_4meta6DoEvalIFNS3_17RemoveAnnotationsENS3_4TypeIT_EEEE4type4typeEv>
+ * mov    %eax,%ebp
+ * test   %rbx,%rbx
+ * je     0x400f42 <_Z4testv+130>
+ * mov    %rbx,%rdi
+ * callq  0x401230 <_ZN5fruit4impl15InjectorStorageD2Ev>
+ * mov    %rbx,%rdi
+ * callq  0x400b10 <_ZdlPv@plt>
+ * mov    %ebp,%eax
+ * add    $0xd0,%rsp
+ * pop    %rbx
+ * pop    %r14
+ * pop    %rbp
+ * retq
+ * mov    %rax,%r14
+ * mov    0xb0(%rsp),%rdi
+ * test   %rdi,%rdi
+ * je     0x400f65 <_Z4testv+165>
+ * callq  0x400b10 <_ZdlPv@plt>
+ * mov    0x98(%rsp),%rdi
+ * test   %rdi,%rdi
+ * je     0x400f77 <_Z4testv+183>
+ * callq  0x400b10 <_ZdlPv@plt>
+ * mov    0x50(%rsp),%rdi
+ * test   %rdi,%rdi
+ * je     0x400fb2 <_Z4testv+242>
+ * callq  0x400b10 <_ZdlPv@plt>
+ * mov    %r14,%rdi
+ * callq  0x400bb0 <_Unwind_Resume@plt>
+ * mov    %rax,%r14
+ * test   %rbx,%rbx
+ * je     0x400fa6 <_Z4testv+230>
+ * mov    %rbx,%rdi
+ * callq  0x401230 <_ZN5fruit4impl15InjectorStorageD2Ev>
+ * mov    %rbx,%rdi
+ * callq  0x400b10 <_ZdlPv@plt>
+ * movq   $0x0,0xc8(%rsp)
+ * mov    %r14,%rdi
+ * callq  0x400bb0 <_Unwind_Resume@plt>
  */
 //->
