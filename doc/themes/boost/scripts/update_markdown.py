fedaee5baec3adfc22c7d50ac1cbd79e6aa69588
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-02-25 14:52:39 +0000

    [doc] boost theme

--- doc/themes/boost/scripts/update_markdown.py
+++ doc/themes/boost/scripts/update_markdown.py
@@ -14,20 +14,19 @@ def print_cpp(filename):
         tmp_ignore = True
       elif line.startswith("//->"):
         tmp_ignore = False
-      if not ignore and not line.startswith("//"):
+      if not ignore and not line.startswith("//") and not line.find("clang-format") != -1:
         print line,
       ignore = tmp_ignore
-  print("```"),
-
+  print("```")
 
 def update_md(filename):
   for line in fileinput.input(filename, inplace=True):
     if line.find("[CPP]") != -1:
       print_cpp("../" + line.split("(")[1][len(sys.argv[2])+1:].split(")")[0])
     elif line.find("[CPP(BTN)]") != -1:
-      name = line.split("(")[2].split("|")[0].replace("_", " ")
-      href = "../" + line.split("|")[1][len(sys.argv[2])+1:].split(")")[0]
-      print "[" + name + "](" + href + ")"
+      name = line.split("(")[2].split("|")[0].replace("_", " ").replace("Run", "Get")
+      href = line.split("|")[1].split(")")[0]
+      print "[[" + name + "](" + href + ")]"
     elif line.find("[CPP(SHOW)]") != -1:
       print_cpp("../" + line.split("(")[2][len(sys.argv[2])+1:].split(")")[0])
     elif line.find("[CPP(SPLIT)]") != -1:
