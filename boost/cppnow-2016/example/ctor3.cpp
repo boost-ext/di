5954eca32b18547bb35bb5fa3a09c28fe7db9e02
Continuous Integration <krzysztof@jusiak.net>
2016-05-01 23:23:13 +0000

    doc update

--- boost/cppnow-2016/example/ctor3.cpp
+++ boost/cppnow-2016/example/ctor3.cpp
@@ -1,3 +1,9 @@
+//
+// Copyright (c) 2012-2016 Krzysztof Jusiak (krzysztof at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <type_traits>
 
 template<class TParent>
