4bc500bacd0b579d05ba5b726ed718d705d7fb02
Kris Jusiak <kris@jusiak.net>
2017-03-26 18:27:11 +0000

    [license] 2012-2017

--- benchmark/generate_test.py
+++ benchmark/generate_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2012-2016 Krzysztof Jusiak (krzysztof at jusiak dot net)
+# Copyright (c) 2012-2017 Kris Jusiak (kris at jusiak dot net)
 #
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
