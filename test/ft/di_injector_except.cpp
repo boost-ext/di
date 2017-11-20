6591fef1e575af931f4aec7ce8a7386620c89bb6
Kanstantsin Chernik <kanstantsin.chernik@gmail.com>
2017-11-20 22:25:28 +0000

    Allow callable instance scope to throw exceptions

--- test/ft/di_injector_except.cpp
+++ test/ft/di_injector_except.cpp
@@ -26,3 +26,80 @@ test should_throw_when_ctor_throws = [] {
   }
   expect(cought);
 };
+
+
+struct empty {};
+
+test should_throw_when_lambda_with_2_args_throws = [] {
+
+	const auto injector = di::make_injector(di::bind<empty>().to([](const auto&, const auto&) {
+		throw 0;
+		return empty{};
+	}));
+
+	auto cought = false;
+	try {
+		injector.create<empty>();
+	}
+	catch (...) {
+		cought = true;
+	}
+	expect(cought);
+};
+
+
+
+test should_throw_when_lambda_with_1_arg_throws = [] {
+
+	const auto injector = di::make_injector(di::bind<empty>().to([](const auto&) {
+		throw 0;
+		return empty{};
+	}));
+
+	auto cought = false;
+	try {
+		injector.create<empty>();
+	}
+	catch (...) {
+		cought = true;
+	}
+	expect(cought);
+};
+
+
+test should_throw_when_lambda_with_no_args_throws = [] {
+
+	const auto injector = di::make_injector(di::bind<empty>().to([]() {
+		throw 0;
+		return empty{};
+	}));
+
+	auto cought = false;
+	try {
+		injector.create<empty>();
+	}
+	catch (...) {
+		cought = true;
+	}
+	expect(cought);
+};
+
+test should_throw_when_factory_with_1_arg_throws = [] {
+	struct except_factory {
+		template <class TInjector>
+		auto operator()(const TInjector&) const {
+			throw 0;
+			return empty{};
+		}
+	};
+	const auto injector = di::make_injector(di::bind<empty>().to(except_factory{}));
+
+	auto cought = false;
+	try {
+		injector.create<empty>();
+	}
+	catch (...) {
+		cought = true;
+	}
+	expect(cought);
+};
