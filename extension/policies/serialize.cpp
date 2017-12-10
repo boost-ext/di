19b2542626d04f2b91c74fc854c09a8685b37a17
Kris <kris@jusiak.net>
2017-12-10 12:53:33 +0000

    :bug: Compilation error when `_GLIBCXX_DEBUG` is defined
    Problem:
    - Forward definitions of std components is compiler specific
    
    Solution:
    - Include a std header if available, otherwise forward declare it as
    it's only required if used. The core DI compiles and works without std
    headers.

--- extension/policies/serialize.cpp
+++ extension/policies/serialize.cpp
@@ -5,6 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 //<-
+#define BOOST_DI_DISABLE_SHARED_PTR_DEDUCTION
 #include <algorithm>
 #include <cassert>
 #include <functional>
