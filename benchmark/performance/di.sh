1c52bb93fc8c60ce88b220c99e47933a713106d6
Kris Jusiak <kris@jusiak.net>
2018-01-05 09:16:29 +0000

    :arrow_up: Update copyright years
    Problem:
    - Copyrights claims 2012-2017 but it's 2018.
    
    Solution:
    - Update to 2012-2018.

--- benchmark/performance/di.sh
+++ benchmark/performance/di.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012-2017 Kris Jusiak (kris at jusiak dot net)
+# Copyright (c) 2012-2018 Kris Jusiak (kris at jusiak dot net)
 #
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
