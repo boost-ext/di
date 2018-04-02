d3c10cc40891fcd0ca62f696c7195f5a93e2a62d
Kris Jusiak <kris@jusiak.net>
2018-04-02 20:12:51 +0000

    :new: [example] Polymorphism examples
    Problem:
    - There are missing example showing off the same configuration for different types of polymorphism.
    
    Solution:
    - Add polymorphism examples.

---
+++ example/polymorphism/inheritance.cpp
@@ -0,0 +1,43 @@
+//
+// Copyright (c) 2012-2018 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <cassert>
+#include <memory>
+#include <sstream>
+
+#include "common/config.hpp"
+
+/*<<Dynamic polymorphism - inheritance>>*/
+class Drawable {
+ public:
+  virtual ~Drawable() noexcept = default;
+  virtual void draw(std::ostream&) const = 0;
+};
+
+class Example {
+ public:
+  explicit Example(std::unique_ptr<const Drawable> drawable) : drawable{std::move(drawable)} {}
+
+  void draw(std::ostream& out) const { drawable->draw(out); }
+
+ private:
+  std::unique_ptr<const Drawable> drawable;
+};
+
+struct Square : Drawable {
+  void draw(std::ostream& out) const override { out << "Square"; }
+};
+
+struct Circle : Drawable {
+  void draw(std::ostream& out) const override { out << "Circle"; }
+};
+
+int main() {
+  std::stringstream str{};
+  auto example = config().create<Example>();
+  example.draw(str);
+  assert("Square" == str.str());
+}
---
+++ example/polymorphism/templates.cpp
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2012-2018 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <cassert>
+#include <sstream>
+
+#include "common/config.hpp"
+
+/*<<Static polymorphism - templates>>*/
+template <typename TDrawable = class Drawable>
+class Example {
+ public:
+  explicit Example(const TDrawable drawable) : drawable(drawable) {}
+
+  void draw(std::ostream& out) const { drawable.draw(out); }
+
+ private:
+  const TDrawable drawable;
+};
+
+struct Square {
+  void draw(std::ostream& out) const { out << "Square"; }
+};
+
+struct Circle {
+  void draw(std::ostream& out) const { out << "Circle"; }
+};
+
+int main() {
+  std::stringstream str{};
+  auto example = config().create<Example>();
+  example.draw(str);
+  assert("Square" == str.str());
+}
