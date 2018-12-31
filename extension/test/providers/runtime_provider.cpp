8306722c5c5ec152d75146b8a238a22b35a90931
Kris Jusiak <kris@jusiak.net>
2018-12-31 09:27:38 -0700

    :white_check_mark: [extension] Support for installing modules in `runtime_provider`
    Problem:
    - Modules can't be installed with the `install` call.
    
    Solution:
    - Add support for installing modules with the `install` call.

--- extension/test/providers/runtime_provider.cpp
+++ extension/test/providers/runtime_provider.cpp
@@ -7,6 +7,7 @@
 #include "boost/di/extension/providers/runtime_provider.hpp"
 
 #include <cassert>
+#include <string>
 
 namespace di = boost::di;
 
@@ -20,23 +21,57 @@ struct i2 {
   virtual int bar() const = 0;
 };
 
+struct i3 {
+  virtual ~i3() noexcept = default;
+  virtual int f() const = 0;
+};
+
+struct i4 {
+  virtual ~i4() noexcept = default;
+  virtual int foo() const = 0;
+};
+
+struct impl3 : i3 {
+  explicit impl3(const int& i) { assert(i == 87); }
+  int f() const override { return 1234; }
+};
+
 struct impl2 : i2 {
+  explicit impl2(i3& i, std::string str) {
+    assert(i.f() == 1234);
+    assert(str == "text");
+  }
   int bar() const override { return 99; }
 };
 
 struct impl1 : i1 {
-  impl1(std::unique_ptr<i2> sp2) { assert(sp2->bar() == 99); }
-
+  explicit impl1(std::unique_ptr<i2> sp2) { assert(sp2->bar() == 99); }
   int foo() const override { return 42; }
 };
 
+struct impl4 : i4 {
+  int foo() const override { return 100; }
+};
+
+class module_example {
+ public:
+  explicit module_example(std::shared_ptr<i4> sp) : sp{sp} {}
+  auto get() const { return sp->foo() * 2; }
+
+ private:
+  std::shared_ptr<i4> sp{};
+};
+
 struct example {
-  example(std::shared_ptr<i1> sp, int i) {
+  example(std::shared_ptr<i1> sp, int i, module_example& me) {
     assert(dynamic_cast<impl1*>(sp.get()));
     assert(sp->foo() == 42);
     assert(i == 87);
+    assert(me.get() == 2 * 100);
   }
 };
+
+auto module = [] { return di::make_injector(di::bind<i4>().to<impl4>()); };
 
 int main() {
   // clang-format off
@@ -53,6 +88,16 @@ int main() {
 
   /*<<more bindings>>*/
   injector.install(di::bind<int>().to(87));
+
+  /*<<injector bindings>>*/
+  auto component = di::make_injector(
+    di::bind<i3>().to<impl3>(),
+    di::bind<std::string>().to("text")
+  );
+  injector.install(component);
+
+  /*<<module bindings>>*/
+  injector.install(module());
 
   /*<<create example>>*/
   injector.create<example>();
