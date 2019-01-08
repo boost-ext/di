64a10dcd023d5719a86642ec16b888b4a1121351
Kris Jusiak <kris@jusiak.net>
2019-01-07 20:02:55 -0700

    :new: [core] `is_creatable`
    Problem:
    - There is no way to check at compile time whether a type is creatable with a given injector.
    
    Solution:
    - Introduce `is_creatable` which allows to check at compile time if the type is creatable.

---
+++ example/is_creatable.cpp
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 2012-2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/di.hpp>
+#include <cassert>
+
+namespace di = boost::di;
+
+struct iworld {
+  virtual ~iworld() noexcept = default;
+  virtual void f() = 0;
+};
+
+struct world : iworld {
+  void f() override {}
+};
+
+struct hello {
+  explicit hello(iworld&) {}
+};
+
+int main() {
+  {
+    const auto injector = di::make_injector(di::bind<iworld>().to<world>());
+    static_assert(di::is_creatable<hello>(injector),  "");
+  }
+
+  {
+    const auto injector = di::make_injector();
+    static_assert(!di::is_creatable<hello>(injector), "iworld is not abstract!");
+  }
+}
